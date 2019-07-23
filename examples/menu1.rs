0410674c4f05c7fb878cc94b2bbac2a5ee2c3d94
Willi Kappler <willi.kappler@uni-tuebingen.de>
2019-07-23 16:41:25 +0200

    Initial code

--- examples/menu1.rs
+++ examples/menu1.rs
@@ -1,6 +1,9 @@
 
-
+use green_moon_2d::prelude::*;
 
 fn main() {
+    let settings = GM_Settings::new();
+    let mut menu_example = GreenMoon2D::new();
 
+    menu_example.set_settings(settings);
 }
