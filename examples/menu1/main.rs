c44c24b2d1bc3376d21f4065e84c051b076159eb
Willi Kappler <willi.kappler@uni-tuebingen.de>
2019-07-23 19:16:07 +0200

    Refactor menu1

--- examples/menu1/main.rs
+++ examples/menu1/main.rs
@@ -1,68 +1,9 @@
 
 use green_moon_2d::prelude::*;
 
-struct MainScreen {
-    state: GM_Screen_State,
-    menu: GM_Menu,
-}
+mod main_screen;
 
-impl MainScreen {
-    fn new() -> MainScreen {
-        let menu = GM_Menu::new();
-
-        MainScreen {
-            state: GM_Screen_State::GM_Enter,
-            menu,
-        }
-    }
-}
-
-impl GM_Screen_T for MainScreen {
-    fn enter(&mut self) {
-        self.state = GM_Screen_State::GM_Enter;
-    }
-
-    fn process(&mut self, runtime: &mut GM_Runtime) {
-        match self.state {
-            GM_Screen_State::GM_Enter => {
-                // Nothing to do for now
-            }
-            GM_Screen_State::GM_Normal => {
-                // Only process when in normal mode
-                self.menu.process(runtime);
-            }
-            GM_Screen_State::GM_Leave => {
-                // Nothing to do for now
-            }
-        }
-    }
-
-    fn update(&mut self, runtime: &mut GM_Runtime) {
-        self.menu.update(runtime);
-
-        match self.state {
-            GM_Screen_State::GM_Enter => {
-            }
-            GM_Screen_State::GM_Normal => {
-            }
-            GM_Screen_State::GM_Leave => {
-            }
-        }
-    }
-
-    fn draw(&mut self, runtime: &mut GM_Runtime) {
-        self.menu.draw(runtime);
-
-        match self.state {
-            GM_Screen_State::GM_Enter => {
-            }
-            GM_Screen_State::GM_Normal => {
-            }
-            GM_Screen_State::GM_Leave => {
-            }
-        }
-    }
-}
+use main_screen::{MainScreen};
 
 
 fn main() {
