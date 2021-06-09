1efa6aa265aac83914a831e270cf500a4fa92d5b
Willi Kappler <grandor@gmx.de>
2021-06-09 17:16:47 +0200

    Add strings as possible values

--- src/value.rs
+++ src/value.rs
@@ -1,8 +1,9 @@
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum GMValue {
     GMNone,
     GMBool(bool),
     GMF32(f32),
     GMUSize(usize),
+    GMString(String),
 }
