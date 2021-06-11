264b257705eb733bbd1dc16d37fd5da3a5cdaf45
Willi Kappler <grandor@gmx.de>
2021-06-11 15:52:29 +0200

    Add sound effects for menu1 example

--- examples/menu1/src/main.rs
+++ examples/menu1/src/main.rs
@@ -1,15 +1,21 @@
 use green_moon_2d::menu::GMMenu;
 use green_moon_2d::font::GMBitmapFont;
 use green_moon_2d::error::GMError;
+use green_moon_2d::sound::GMSound;
 
 use macroquad::prelude::*;
+
+use std::thread;
+use std::time::Duration;
 
 #[macroquad::main("Menu1")]
 async fn main() -> Result<(), GMError> {
     let font = GMBitmapFont::new_rc("../assets/gfx/fonts/cuddly.png", 32.0, 32.0, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.!0123456789?()<>- ").await?;
+    let change_sound = GMSound::new_rc("../assets/sfx/change1.ogg").await?;
+    let enter_sound = GMSound::new_rc("../assets/sfx/enter1.ogg").await?;
 
     let items = ["START", "CONTROLS", "GFX OPTIONS", "SFX OPTIONS", "HIGH SCORE", "CREDITS", "EXIT"];
-    let mut main_menu = GMMenu::new_simple(240.0, 100.0, "MAIN MENU", &items, &font);
+    let mut main_menu = GMMenu::new_simple(240.0, 100.0, "MAIN MENU", &items, &font, &change_sound, &enter_sound);
 
     loop {
         clear_background(BLACK);
@@ -27,6 +33,9 @@ async fn main() -> Result<(), GMError> {
 
         next_frame().await
     }
+
+    println!("Good bye!");
+    thread::sleep(Duration::from_millis(500));
 
     Ok(())
 }
