857e5192db8f61ce9c13392406d8694d52c32a51
Willi Kappler <willi.kappler.gm@gmail.com>
2021-06-18 01:01:41 +0200

    Add more functions

--- src/resource_manager.rs
+++ src/resource_manager.rs
@@ -1,6 +1,6 @@
 
 
-use crate::font::{GMBitmapFont, GMFontT};
+use crate::font::GMFontT;
 use crate::spritesheet::GMSpriteSheet;
 use crate::sound::GMSound;
 use crate::animation::{GMAnimationT};
@@ -12,7 +12,7 @@ pub struct GMResourceManager {
     fonts: HashMap<String, Rc<dyn GMFontT>>,
     sprite_sheets: HashMap<String, Rc<GMSpriteSheet>>,
     sounds: HashMap<String, Rc<GMSound>>,
-    animations: HashMap<String, Rc<dyn GMAnimationT>>,
+    animations: HashMap<String, Box<dyn GMAnimationT>>,
 }
 
 impl GMResourceManager {
@@ -29,7 +29,7 @@ impl GMResourceManager {
         let result = "";
         Self::new_from_json(result)
     }
-    pub fn new_from_json(json: &str) -> Self{
+    pub fn new_from_json(json: &str) -> Self {
         // TODO: create from JSON string
         let mut result = Self::new();
         result
@@ -37,29 +37,43 @@ impl GMResourceManager {
     pub fn add_font<T: 'static + GMFontT>(&mut self, name: &str, font: T) {
         self.fonts.insert(name.to_string(), Rc::new(font));
     }
-    pub fn fonts_from_file(file_name: &str) -> Self {
+    pub fn fonts_from_file(&mut self, file_name: &str) {
         // TODO: read in font file
         let result = "";
-        Self::fonts_from_json(result)
+        self.fonts_from_json(result);
     }
-    pub fn fonts_from_json(json: &str) -> Self {
-        // TODO: parse frons from JSON string
-        let mut result = Self::new();
-
-        result
+    pub fn fonts_from_json(&mut self, json: &str) {
+        // TODO: create frons from JSON string
+    }
+    pub fn get_font(&self, name: &str) -> Option<Rc<dyn GMFontT>> {
+        self.fonts.get(name).map(|v| v.clone())
     }
     pub fn add_sprite_sheet(&mut self, name: &str, sprite_sheet: GMSpriteSheet) {
         self.sprite_sheets.insert(name.to_string(), Rc::new(sprite_sheet));
     }
-    pub fn sprite_sheets_from_file(file_name: &str) -> Self  {
+    pub fn sprite_sheets_from_file(&mut self, file_name: &str) {
         // TODO: read multiple sprite sheets from file
         let result = "";
-        Self::sprite_sheets_from_json(result)
+        self.sprite_sheets_from_json(result);
     }
-    pub fn sprite_sheets_from_json(json: &str) -> Self {
-        // TODO: create sprite sheets from json string
-        let mut result = Self::new();
-
-        result
+    pub fn sprite_sheets_from_json(&mut self, json: &str) {
+        // TODO: create sprite sheets from JSON string
+    }
+    pub fn get_sprite_sheet(&self, name: &str) -> Option<Rc<GMSpriteSheet>> {
+        self.sprite_sheets.get(name).map(|v| v.clone())
+    }
+    pub fn add_animation<T: 'static + GMAnimationT>(&mut self, name: &str, animation: T) {
+        self.animations.insert(name.to_string(), Box::new(animation));
+    }
+    pub fn animations_from_file(&mut self, file_name: &str) {
+        // TODO: read in animations from file
+        let mut result = "";
+        self.animation_from_json(result);
+    }
+    pub fn animation_from_json(&mut self, json: &str) {
+        // TODO: create animation from JSON string
+    }
+    pub fn get_animation(&self, name: &str) -> Option<Box<dyn GMAnimationT>> {
+        self.animations.get(name).map(|v| v.clone_animation())
     }
 }
