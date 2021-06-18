9a5cc9f8abf33af99082264f2a9f7d20998fcdcb
Willi Kappler <grandor@gmx.de>
2021-06-18 17:53:11 +0200

    Add load_string()

--- src/resource_manager.rs
+++ src/resource_manager.rs
@@ -1,9 +1,12 @@
 
 
+use crate::error::GMError;
 use crate::font::GMFontT;
 use crate::spritesheet::GMSpriteSheet;
 use crate::sound::GMSound;
-use crate::animation::{GMAnimationT};
+use crate::animation::GMAnimationT;
+
+use macroquad::file::load_string;
 
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -24,26 +27,27 @@ impl GMResourceManager {
             animations: HashMap::new(),
         }
     }
-    pub fn new_from_file(file_name: &str) -> Self {
+    pub async fn new_from_file(file_name: &str) -> Result<Self, GMError> {
         // TODO: read resources from file
-        let result = "";
-        Self::new_from_json(result)
+        let result = load_string(file_name).await?;
+        Self::new_from_json(&result)
     }
-    pub fn new_from_json(json: &str) -> Self {
+    pub fn new_from_json(json: &str) -> Result<Self, GMError> {
         // TODO: create from JSON string
         let mut result = Self::new();
-        result
+        Ok(result)
     }
     pub fn add_font<T: 'static + GMFontT>(&mut self, name: &str, font: T) {
         self.fonts.insert(name.to_string(), Rc::new(font));
     }
-    pub fn fonts_from_file(&mut self, file_name: &str) {
+    pub async fn fonts_from_file(&mut self, file_name: &str) -> Result<(), GMError>{
         // TODO: read in font file
-        let result = "";
-        self.fonts_from_json(result);
+        let result = load_string(file_name).await?;
+        self.fonts_from_json(&result)
     }
-    pub fn fonts_from_json(&mut self, json: &str) {
+    pub fn fonts_from_json(&mut self, json: &str) -> Result<(), GMError> {
         // TODO: create frons from JSON string
+        Ok(())
     }
     pub fn get_font(&self, name: &str) -> Option<Rc<dyn GMFontT>> {
         self.fonts.get(name).map(|v| v.clone())
@@ -51,13 +55,14 @@ impl GMResourceManager {
     pub fn add_sprite_sheet(&mut self, name: &str, sprite_sheet: GMSpriteSheet) {
         self.sprite_sheets.insert(name.to_string(), Rc::new(sprite_sheet));
     }
-    pub fn sprite_sheets_from_file(&mut self, file_name: &str) {
+    pub async fn sprite_sheets_from_file(&mut self, file_name: &str) -> Result<(), GMError> {
         // TODO: read multiple sprite sheets from file
-        let result = "";
-        self.sprite_sheets_from_json(result);
+        let result = load_string(file_name).await?;
+        self.sprite_sheets_from_json(&result)
     }
-    pub fn sprite_sheets_from_json(&mut self, json: &str) {
+    pub fn sprite_sheets_from_json(&mut self, json: &str) -> Result<(), GMError> {
         // TODO: create sprite sheets from JSON string
+        Ok(())
     }
     pub fn get_sprite_sheet(&self, name: &str) -> Option<Rc<GMSpriteSheet>> {
         self.sprite_sheets.get(name).map(|v| v.clone())
@@ -65,15 +70,32 @@ impl GMResourceManager {
     pub fn add_animation<T: 'static + GMAnimationT>(&mut self, name: &str, animation: T) {
         self.animations.insert(name.to_string(), Box::new(animation));
     }
-    pub fn animations_from_file(&mut self, file_name: &str) {
+    pub async fn animations_from_file(&mut self, file_name: &str) -> Result<(), GMError> {
         // TODO: read in animations from file
-        let mut result = "";
-        self.animation_from_json(result);
+        let result = load_string(file_name).await?;
+        self.animation_from_json(&result)
     }
-    pub fn animation_from_json(&mut self, json: &str) {
+    pub fn animation_from_json(&mut self, json: &str) -> Result<(), GMError> {
         // TODO: create animation from JSON string
+        Ok(())
     }
     pub fn get_animation(&self, name: &str) -> Option<Box<dyn GMAnimationT>> {
         self.animations.get(name).map(|v| v.clone_animation())
+    }
+    pub fn add_sound(&mut self, name: &str, sound: GMSound) {
+        self.sounds.insert(name.to_string(), Rc::new(sound));
+    }
+    pub async fn sounds_from_file(&mut self, file_name: &str) -> Result<(), GMError> {
+        // TODO: read in sound file
+        let result = load_string(file_name).await?;
+        self.sounds_from_json(&result)
+
+    }
+    pub fn sounds_from_json(&mut self, json: &str) -> Result<(), GMError> {
+        // TODO: create sounds from JSON string
+        Ok(())
+    }
+    pub fn get_sound(&self, name: &str) -> Option<Rc<GMSound>> {
+        self.sounds.get(name).map(|v| v.clone())
     }
 }
