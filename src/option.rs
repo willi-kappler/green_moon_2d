5fa02649cd4c4b7fb881ed5024b89c3ed17fe635
Willi Kappler <willi.kappler.gm@gmail.com>
2021-06-27 22:33:16 +0200

    Add option

---
+++ src/option.rs
@@ -0,0 +1,9 @@
+
+pub struct GMOption {
+    sound_volume: f32,
+    music_volume: f32,
+    screen_width: f32,
+    screen_height: f32,
+    language: String,
+    // TODO: keyboard / controller options
+}
