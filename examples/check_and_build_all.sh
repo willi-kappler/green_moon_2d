8b23fd616d02baae4b749489885b88211b9090ab
Willi Kappler <willi.kappler.gm@gmail.com>
2021-06-29 18:29:40 +0200

    Add helper bash script

---
+++ examples/check_and_build_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd bullet_factory/
+cargo check
+cargo build --release
+cd ..
+
+cd menu1
+cargo check
+cargo build --release
+cd ..
+
+cd menu2
+cargo check
+cargo build --release
+cd ..
+
+cd particle1
+cargo check
+cargo build --release
+cd ..
+
+cd scenes1
+cargo check
+cargo build --release
+cd ..
+
