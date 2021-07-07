e8ef0a284a1882084481bc32b2edd5d449745cb1
Willi Kappler <grandor@gmx.de>
2021-07-07 15:42:11 +0200

    Fix angle calculation

--- src/sprite.rs
+++ src/sprite.rs
@@ -19,19 +19,27 @@ pub fn in_rect(x1: f32, x2: f32, y1: f32, y2: f32, xp: f32, yp: f32) -> bool {
     between(x1, xp, x2) && between(y1, yp, y2)
 }
 
-pub fn dist_angle(x1: f32, y1: f32, x2: f32, y2: f32) -> (f32, f32) {
+pub fn dist(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
     let dx = x2 - x1;
     let dy = y2 - y1;
-    let dist = dx.hypot(dy);
-    let angle = (dy / dx).atan();
-
-    (dist, angle)
+    dx.hypot(dy)
 }
 
 pub fn angle(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
     let dx = x2 - x1;
     let dy = y2 - y1;
-    (dy / dx).atan()
+
+    let mut angle = (dy / dx).atan();
+
+    if dx < 0.0 && dy >= 0.0 {
+        angle += consts::PI;
+    } else if dx < 0.0 && dy < 0.0 {
+        angle += consts::PI;
+    } else if dx >= 0.0 && dy < 0.0 {
+        angle += consts::TAU;
+    }
+
+    angle
 }
 
 #[derive(Copy, Clone, PartialEq, Debug)]
