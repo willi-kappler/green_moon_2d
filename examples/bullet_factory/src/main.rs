699ca61a4be1f3cbf032f5f1001fc32ce7c06687
Willi Kappler <grandor@gmx.de>
2021-07-07 15:43:07 +0200

    Add ship and mouse controll

--- examples/bullet_factory/src/main.rs
+++ examples/bullet_factory/src/main.rs
@@ -1,4 +1,3 @@
-use green_moon_2d::sprite::GMSprite;
 use green_moon_2d::error::GMError;
 use green_moon_2d::resource_manager::GMResourceManager;
 use green_moon_2d::bullet_factory::GMBulletFactory;
@@ -13,8 +12,30 @@ async fn main() -> Result<(), GMError> {
 
     let resources = GMResourceManager::new_from_file("resources.json").await?;
 
+    let bullet = resources.get_sprite("bullet1").unwrap();
+    let mut bullet_factory = GMBulletFactory::new(bullet, 20);
+
+    let mut player = resources.get_sprite("ship1").unwrap().clone();
+    player.set_x(400.0);
+    player.set_y(300.0);
+
+    show_mouse(true);
+
     loop {
         clear_background(BLACK);
+
+        bullet_factory.draw();
+        player.draw();
+
+        bullet_factory.update();
+        player.update();
+
+        let (mousex, mousey) = mouse_position();
+        player.rotate_to_point(mousex, mousey);
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            
+        }
 
         if is_key_pressed(KeyCode::Escape) {
             break;
