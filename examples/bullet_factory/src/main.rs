46abed408ffc4220380e1cc78e988e189771fddc
Willi Kappler <grandor@gmx.de>
2021-07-07 16:29:35 +0200

    Add rotation and mid arguments for add_bullet()

--- examples/bullet_factory/src/main.rs
+++ examples/bullet_factory/src/main.rs
@@ -14,6 +14,8 @@ async fn main() -> Result<(), GMError> {
 
     let bullet = resources.get_sprite("bullet1").unwrap();
     let mut bullet_factory = GMBulletFactory::new(bullet, 20);
+    bullet_factory.set_delay(0.1);
+    let bullet_speed = 4.0;
 
     let mut player = resources.get_sprite("ship1").unwrap().clone();
     player.set_x(400.0);
@@ -34,7 +36,10 @@ async fn main() -> Result<(), GMError> {
         player.rotate_to_point(mousex, mousey);
 
         if is_mouse_button_pressed(MouseButton::Left) {
-            
+            let rotation = player.get_rotation();
+            let bullet_vx = rotation.cos() * bullet_speed;
+            let bullet_vy = rotation.sin() * bullet_speed;
+            bullet_factory.add_bullet(player.get_mid_x(), player.get_mid_y(), bullet_vx, bullet_vy, rotation, true);
         }
 
         if is_key_pressed(KeyCode::Escape) {
