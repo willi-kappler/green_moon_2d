892d926055aa634a5da257efc7e11d64b0121ca8
Willi Kappler <grandor@gmx.de>
2021-07-07 13:17:45 +0200

    Getter for option values

--- src/option.rs
+++ src/option.rs
@@ -7,3 +7,30 @@ pub struct GMOption {
     language: String,
     // TODO: keyboard / controller options
 }
+
+impl GMOption {
+    pub fn new() -> Self {
+        Self {
+            sound_volume: 1.0,
+            music_volume: 1.0,
+            screen_width: 800.0,
+            screen_height: 600.0,
+            language: "eng".to_string(),
+        }
+    }
+    pub fn get_sound_volume(&self) -> f32 {
+        self.sound_volume
+    }
+    pub fn get_music_volume(&self) -> f32 {
+        self.music_volume
+    }
+    pub fn get_screen_width(&self) -> f32 {
+        self.screen_width
+    }
+    pub fn get_screen_height(&self) -> f32 {
+        self.screen_height
+    }
+    pub fn get_language(&self) -> &str {
+        &self.language
+    }
+}
