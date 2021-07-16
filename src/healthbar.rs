a402e1fbd87664ae7c8f9cd19a1fe5dfb0a4ce01
Willi Kappler <grandor@gmx.de>
2021-07-16 17:40:55 +0200

    More code, refactor, split up to label and bar

--- src/healthbar.rs
+++ src/healthbar.rs
@@ -1,3 +1,7 @@
+use crate::animation::GMAnimation;
+use crate::spritesheet::GMSpriteSheet;
+use crate::text::GMText;
+
 
 pub trait GMHealthBarT {
     fn draw(&self);
@@ -50,18 +54,94 @@ impl GMHealthBar {
     }
 }
 
-pub struct GMHealthBarTextText {
-
+pub trait GMLabelT {
+    fn draw(&self);
+    fn update(&mut self);
+    fn set_x(&mut self, x: f32);
+    fn set_y(&mut self, y: f32);
+    fn get_x(&self) -> f32;
+    fn get_y(&self) -> f32;
 }
 
-pub struct GMHealthBarTextSprite {
-
+pub struct GMLabel {
+    label: Box<dyn GMLabelT>,
 }
 
-pub struct GMHealthBarSpriteText {
-
+impl GMLabel {
+    pub fn draw(&self) {
+        self.label.draw();
+    }
+    pub fn update(&mut self) {
+        self.label.update();
+    }
+    pub fn set_x(&mut self, x: f32) {
+        self.label.set_x(x);
+    }
+    pub fn set_y(&mut self, y: f32) {
+        self.label.set_y(y);
+    }
+    pub fn get_x(&self) -> f32 {
+        self.label.get_x()
+    }
+    pub fn get_y(&self) -> f32 {
+        self.label.get_y()
+    }
 }
 
-pub struct GMHealthBarSpriteSprite {
+pub struct GMLabelText {
+    text: GMText,
+}
 
+pub struct GMLabelSprite {
+    x: f32,
+    y: f32,
+    sprite_sheet: GMSpriteSheet,
+    animation: GMAnimation,
+}
+
+pub trait GMBarT {
+    fn draw(&self);
+    fn update(&mut self);
+    fn set_x(&mut self, x: f32);
+    fn set_y(&mut self, y: f32);
+    fn get_x(&self) -> f32;
+    fn get_y(&self) -> f32;
+    fn set_value(&mut self, value: u32);
+}
+
+pub struct GMBar {
+    bar: Box<dyn GMBarT>,
+}
+
+impl GMBar {
+    pub fn draw(&self) {
+        self.bar.draw();
+    }
+    pub fn update(&mut self) {
+        self.bar.update();
+    }
+    pub fn set_x(&mut self, x: f32) {
+        self.bar.set_x(x);
+    }
+    pub fn set_y(&mut self, y: f32) {
+        self.bar.set_y(y);
+    }
+    pub fn get_x(&self) -> f32 {
+        self.bar.get_x()
+    }
+    pub fn get_y(&self) -> f32 {
+        self.bar.get_y()
+    }
+    pub fn set_value(&mut self, value: u32) {
+        self.bar.set_value(value);
+    }
+}
+
+
+pub struct GMHealthBarSimple {
+    x: f32,
+    y: f32,
+    health: u32,
+    label: GMLabel,
+    bar: GMBar,
 }
