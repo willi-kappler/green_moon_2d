f7d107b83cabb6edf887a45d3d9c4dcfcdb14546
Willi Kappler <willi.kappler.gm@gmail.com>
2022-08-04 19:30:13 +0200

    Add some text effects

--- src/bitmap_text.rs
+++ src/bitmap_text.rs
@@ -4,12 +4,10 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::fmt::Debug;
 use std::any::Any;
-
-use log::error;
+use std::f32::consts::TAU;
 
 use crate::texture::GMTexture;
 use crate::context::GMContext;
-use crate::math::GMVec2D;
 use crate::util::error_panic;
 
 #[derive(Debug, Clone)]
@@ -36,19 +34,23 @@ impl GMBitmapFont {
         self.texture.get_unit_dimension()
     }
 
-    pub fn draw(&self, c: char, x: f32, y: f32, context: &mut GMContext) {
-        self.draw_opt(c, x, y, 0.0, false, false, context);
-    }
-
-    pub fn draw_opt(&self, c: char, x: f32, y: f32, angle: f32, flip_x: bool, flip_y: bool, context: &mut GMContext) {
+    pub fn get_index(&self, c: char) -> u32 {
         match self.mapping.get(&c) {
             Some(index) => {
-                self.texture.draw_opt(x, y, *index, angle, flip_x, flip_y, context);
+                *index
             }
             None => {
                 error_panic(&format!("GMBitmapFont::draw_opt(), Character '{}' not in map.", c));
             }
         }
+    }
+
+    pub fn draw(&self, index: u32, x: f32, y: f32, context: &mut GMContext) {
+        self.draw_opt(index, x, y, 0.0, false, false, context);
+    }
+
+    pub fn draw_opt(&self, index: u32, x: f32, y: f32, angle: f32, flip_x: bool, flip_y: bool, context: &mut GMContext) {
+        self.texture.draw_opt(x, y, index, angle, flip_x, flip_y, context);
     }
 }
 
@@ -57,42 +59,147 @@ impl GMBitmapFont {
 pub struct GMBitmapText {
     pub font: Rc<GMBitmapFont>,
     pub text: String,
-    pub position: GMVec2D,
+    pub base_x: f32,
+    pub base_y: f32,
     pub spacing_x: f32,
     pub spacing_y: f32,
     pub horizontal: bool,
+    pub chars: Vec<(u32, f32, f32, f32)>, // index, x, y, angle
 }
 
 impl GMBitmapText {
     pub fn new(font: Rc<GMBitmapFont>, text: &str, x: f32, y: f32) -> Self {
-        Self {
+        let mut text = Self {
             font,
             text: text.to_string(),
-            position: GMVec2D::new(x, y),
+            base_x: x,
+            base_y: y,
             spacing_x: 0.0,
             spacing_y: 0.0,
             horizontal: true,
-        }
+            chars: Vec::new(),
+        };
+
+        text.reset_chars();
+
+        text
     }
 
     pub fn new2(font: &str, text: &str, x: f32, y: f32, context: &GMContext) -> Self {
         Self::new(context.resources.get_font_clone(font), text, x, y)
     }
+
+    pub fn reset_chars(&mut self) {
+        if self.horizontal {
+            let mut x = self.base_x;
+            let (dx, _) = self.font.get_char_dimensions();
+            self.chars.clear();
+
+            for c in self.text.chars() {
+                let index = self.font.get_index(c);
+                self.chars.push((index, x, self.base_y, 0.0));
+                x += dx + self.spacing_x;
+            }
+        } else {
+            let mut y = self.base_y;
+            let (_, dy) = self.font.get_char_dimensions();
+            self.chars.clear();
+
+            for c in self.text.chars() {
+                let index = self.font.get_index(c);
+                self.chars.push((index, self.base_x, y, 0.0));
+                y += dy + self.spacing_y;
+            }
+        }
+    }
 }
 
-pub trait GMTextEffect {
-    fn update(&mut self, _context: &mut GMContext) {
-        error_panic("GMTextEffect::update() not implemented");
+pub trait GMTextEffectT {
+    fn update(&mut self, _text: &mut GMBitmapText, _context: &mut GMContext) {
     }
 
-    fn draw(&self, _context: &mut GMContext) {
-        error_panic("GMTextEffect::draw() not implemented");
+    fn draw(&self, _text: &GMBitmapText, _context: &mut GMContext) {
     }
 
     fn send_message(&mut self, _message: &str, _data: Option<Box<dyn Any>>, _context: &mut GMContext) {
-        error_panic("GMTextEffect::send_message() not implemented");
     }
 }
 
-// TODO: Add text effects
+pub struct GMTextEffectDraw {
+}
 
+impl GMTextEffectDraw {
+    pub fn new() -> Self {
+        Self { }
+    }
+}
+
+impl GMTextEffectT for GMTextEffectDraw {
+    fn draw(&self, text: &GMBitmapText, context: &mut GMContext) {
+        for (index, x, y, angle) in text.chars.iter() {
+            text.font.draw_opt(*index, *x, *y, *angle, false, false, context);
+        }
+    }
+
+}
+
+pub struct GMTextEffectReset {
+}
+
+impl GMTextEffectReset {
+    pub fn new() -> Self {
+        Self { }
+    }
+}
+
+impl GMTextEffectT for GMTextEffectReset {
+    fn update(&mut self, text: &mut GMBitmapText, _context: &mut GMContext) {
+        text.reset_chars();
+    }
+}
+
+pub struct GMTextEffectWave {
+    pub amplitude: f32,
+    pub speed: f32,
+    pub offset: f32,
+    pub time: f32,
+}
+
+impl GMTextEffectWave {
+    pub fn new(amplitude: f32, speed: f32, offset: f32) -> Self {
+        Self {
+            amplitude,
+            speed,
+            offset,
+            time: 0.0,
+        }
+    }
+}
+
+impl GMTextEffectT for GMTextEffectWave {
+    fn update(&mut self, text: &mut GMBitmapText, _context: &mut GMContext) {
+        let mut offset = 0.0;
+
+        if text.horizontal {
+            for (_, _, y, _) in text.chars.iter_mut() {
+                *y = text.base_y + (self.amplitude * (self.time + offset).sin());
+                offset += self.offset;
+            }
+        } else {
+            for (_, x, _, _) in text.chars.iter_mut() {
+                *x = text.base_x + (self.amplitude * (self.time + offset).sin());
+                offset += self.offset;
+            }
+        }
+
+        self.time += self.speed;
+
+        if self.time > TAU {
+            self.time -= TAU;
+        }
+    }
+
+    fn send_message(&mut self, message: &str, data: Option<Box<dyn Any>>, _context: &mut GMContext) {
+
+    }
+}
