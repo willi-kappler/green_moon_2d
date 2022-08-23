c6333b562c2681ca18b71f4bc92c3cd0a613c86e
Willi Kappler <grandor@gmx.de>
2022-08-23 18:19:05 +0200

    Add more code for the text1 example

--- examples/text1/src/main.rs
+++ examples/text1/src/main.rs
@@ -2,10 +2,10 @@
 
 use std::fs::File;
 
-use log::{debug};
+// use log::{debug};
 use simplelog::{WriteLogger, LevelFilter, ConfigBuilder};
 
-use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMSceneMessage, GMSceneReply};
+use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMBitmapText};
 
 #[derive(Clone, Debug)]
 struct TextScene1 {
@@ -19,33 +19,34 @@ impl TextScene1 {
 }
 
 impl GMSceneT for TextScene1 {
-    fn send_message(&mut self, message: GMSceneMessage, context: &mut GMContext) -> GMSceneReply {
-        use GMSceneMessage::*;
+    fn get_name(&self) -> &str {
+        "text_scene1"
+    }
 
-        match message {
-            Update => {
-                if context.input.key_esc_down() {
-                    debug!("ESC key pressed");
-                    context.quit();
-                }        
-            }
-            _ => {
-                debug!("Unhandled message: {:?}", message);
-            }
+    fn update(&mut self, context: &mut GMContext) {
+        if context.input.key_esc_down() {
+            context.quit();
         }
+    }
 
-        GMSceneReply::Empty
+    fn draw(&self, context: &mut GMContext) {
+        context.clear_black();
+
+
+
     }
 }
 
 fn main() {
     let config = ConfigBuilder::new().build();
-    let _simple_log = WriteLogger::init(LevelFilter::Debug, config, File::create("test1.log").expect("Could not create log file"));
+    let _simple_log = WriteLogger::init(LevelFilter::Debug, config, File::create("text1.log").expect("Could not create log file"));
 
     let text1_scene = TextScene1::new();
 
     let mut engine = GMEngine::new();
+    engine.load_configuration("config.json");
     engine.init();
-    engine.add_scene("text1", text1_scene);
+    engine.load_resources("resources.json");
+    engine.add_scene(text1_scene);
     engine.run();
 }
