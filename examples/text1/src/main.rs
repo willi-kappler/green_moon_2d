ae557e82964c98916ac97a50ab404200521b831a
Willi Kappler <grandor@gmx.de>
2022-08-25 14:19:17 +0200

    Refactor

--- examples/text1/src/main.rs
+++ examples/text1/src/main.rs
@@ -11,24 +11,23 @@ use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMBitmapText, GMBitmapFont};
 #[derive(Clone, Debug)]
 struct TextScene1 {
     name: String,
-    title: GMBitmapText,
-    description: GMBitmapText,
-    font1: GMBitmapText,
-    font2: GMBitmapText,
-    font3: GMBitmapText,
+    texts: Vec<GMBitmapText>,
 }
 
 impl TextScene1 {
     pub fn new(font: &Rc<GMBitmapFont>) -> Self {
         const space: f32 = 50.0;
+        let mut texts = Vec::new();
+
+        texts.push(GMBitmapText::new(font, "TEXT TEST 1", 336.0, 32.0));
+        texts.push(GMBitmapText::new(font, "PRESS NUMBER TO CHANGE FONT", 32.0, 32.0 + (1.0 * space)));
+        texts.push(GMBitmapText::new(font, "1 - BBC", 32.0, 32.0 + (2.0 * space)));
+        texts.push(GMBitmapText::new(font, "2 - BLAGGER", 32.0, 32.0 + (3.0 * space)));
+        texts.push(GMBitmapText::new(font, "3 - CUDDLY", 32.0, 32.0 + (4.0 * space)));
 
         Self {
             name: "text_scene1".to_string(),
-            title: GMBitmapText::new(font, "TEXT TEST 1", 336.0, 32.0),
-            description: GMBitmapText::new(font, "PRESS NUMBER TO CHANGE FONT", 32.0, 32.0 + (1.0 * space)),
-            font1: GMBitmapText::new(font, "1 - BBC", 32.0, 32.0 + (2.0 * space)),
-            font2: GMBitmapText::new(font, "2 - BLAGGER", 32.0, 32.0 + (3.0 * space)),
-            font3: GMBitmapText::new(font, "3 - CUDDLY", 32.0, 32.0 + (4.0 * space)),
+            texts,
         }
     }
 }
@@ -46,42 +45,34 @@ impl GMSceneT for TextScene1 {
         if context.input.key_1_up() {
             let font = context.resources.get_font_clone("font_bbc");
 
-            self.title.set_font(&font);
-            self.description.set_font(&font);
-            self.font1.set_font(&font);
-            self.font2.set_font(&font);
-            self.font3.set_font(&font);
+            for text in self.texts.iter_mut() {
+                text.set_font(&font);
+            }
         }
 
         if context.input.key_2_up() {
             let font = context.resources.get_font_clone("font_blagger");
 
-            self.title.set_font(&font);
-            self.description.set_font(&font);
-            self.font1.set_font(&font);
-            self.font2.set_font(&font);
-            self.font3.set_font(&font);
+            for text in self.texts.iter_mut() {
+                text.set_font(&font);
+            }
         }
 
         if context.input.key_3_up() {
             let font = context.resources.get_font_clone("font_cuddly");
 
-            self.title.set_font(&font);
-            self.description.set_font(&font);
-            self.font1.set_font(&font);
-            self.font2.set_font(&font);
-            self.font3.set_font(&font);
+            for text in self.texts.iter_mut() {
+                text.set_font(&font);
+            }
         }
     }
 
     fn draw(&self, context: &mut GMContext) {
         context.clear_black();
 
-        self.title.draw(context);
-        self.description.draw(context);
-        self.font1.draw(context);
-        self.font2.draw(context);
-        self.font3.draw(context);
+        for text in self.texts.iter() {
+            text.draw(context);
+        }
     }
 }
 
