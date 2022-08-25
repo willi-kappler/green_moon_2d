925705488b019e556aa727726b06bd1a13f62dbb
Willi Kappler <grandor@gmx.de>
2022-08-25 16:19:08 +0200

    Better event handling

--- src/input.rs
+++ src/input.rs
@@ -1,23 +1,50 @@
 
 use std::fmt::Debug;
 
-use sdl2::keyboard::Keycode;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::event::WindowEvent;
 use sdl2::EventPump;
 
+#[derive(Copy, Clone, Debug)]
+pub enum GMEventCode {
+    Quit = 0,
+    WindowClose,
+    KeyESCDown,
+    KeyESCUp,
+    Key0Down,
+    Key0Up,
+    Key1Down,
+    Key1Up,
+    Key2Down,
+    Key2Up,
+    Key3Down,
+    Key3Up,
+    Key4Down,
+    Key4Up,
+    Key5Down,
+    Key5Up,
+    Key6Down,
+    Key6Up,
+    Key7Down,
+    Key7Up,
+    Key8Down,
+    Key8Up,
+    Key9Down,
+    Key9Up,
+    KeyUpDown,
+    KeyUpUp,
+    KeyDownDown,
+    KeyDownUp,
+    KeyLeftDown,
+    KeyLeftUp,
+    KeyRightDown,
+    KeyRightUp,
+}
 
 pub struct GMInput {
     event_pump: EventPump,
-    key_esc_down_: bool,
-    key_esc_up_: bool,
-    key_0_down_: bool,
-    key_0_up_: bool,
-    key_1_down_: bool,
-    key_1_up_: bool,
-    key_2_down_: bool,
-    key_2_up_: bool,
-    key_3_down_: bool,
-    key_3_up_: bool,
+    events: Vec<bool>,
 }
 
 
@@ -31,107 +58,111 @@ impl GMInput {
     pub fn new(event_pump: EventPump) -> Self {
         Self {
             event_pump,
-            key_esc_down_: false,
-            key_esc_up_: false,
-            key_0_down_: false,
-            key_0_up_: false,
-            key_1_down_: false,
-            key_1_up_: false,
-            key_2_down_: false,
-            key_2_up_: false,
-            key_3_down_: false,
-            key_3_up_: false,
+            events: vec![false; 100],
         }
+    }
+
+    pub fn is(&self, event_code: GMEventCode) -> bool {
+        self.events[event_code as usize]
     }
 
     pub(crate) fn update(&mut self) {
-        self.key_esc_down_ = false;
-        self.key_esc_up_ = false;
-        self.key_0_down_ = false;
-        self.key_0_up_ = false;
-        self.key_1_down_ = false;
-        self.key_1_up_ = false;
-        self.key_2_down_ = false;
-        self.key_2_up_ = false;
-        self.key_3_down_ = false;
-        self.key_3_up_ = false;
+        for event in self.events.iter_mut() {
+            *event = false;
+        }
 
         for event in self.event_pump.poll_iter() {
             match event {
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    self.key_esc_down_ = true;
+                Event::Quit{..} => {
+                    self.events[GMEventCode::Quit as usize] = true;
                 }
-                Event::KeyUp { keycode: Some(Keycode::Escape), .. } => {
-                    self.key_esc_up_ = true;
+                Event::Window{win_event: WindowEvent::Close, ..} => {
+                    self.events[GMEventCode::WindowClose as usize] = true;
                 }
-                Event::KeyDown { keycode: Some(Keycode::Num0), .. } => {
-                    self.key_0_down_ = true;
+                Event::KeyDown{keycode: Some(keycode), ..} => {
+                    match keycode {
+                        Keycode::Escape => {
+                            self.events[GMEventCode::KeyESCDown as usize] = true;
+                        }
+                        Keycode::Num0 => {
+                            self.events[GMEventCode::Key0Down as usize] = true;
+                        }
+                        Keycode::Num1 => {
+                            self.events[GMEventCode::Key1Down as usize] = true;
+                        }
+                        Keycode::Num2 => {
+                            self.events[GMEventCode::Key2Down as usize] = true;
+                        }
+                        Keycode::Num3 => {
+                            self.events[GMEventCode::Key3Down as usize] = true;
+                        }
+                        Keycode::Num4 => {
+                            self.events[GMEventCode::Key4Down as usize] = true;
+                        }
+                        Keycode::Num5 => {
+                            self.events[GMEventCode::Key5Down as usize] = true;
+                        }
+                        Keycode::Num6 => {
+                            self.events[GMEventCode::Key6Down as usize] = true;
+                        }
+                        Keycode::Num7 => {
+                            self.events[GMEventCode::Key7Down as usize] = true;
+                        }
+                        Keycode::Num8 => {
+                            self.events[GMEventCode::Key8Down as usize] = true;
+                        }
+                        Keycode::Num9 => {
+                            self.events[GMEventCode::Key9Down as usize] = true;
+                        }
+                        _ => {
+                            // Unsupported keycode for now
+                        }
+                    }
                 }
-                Event::KeyUp { keycode: Some(Keycode::Num0), .. } => {
-                    self.key_0_up_ = true;
-                }
-                Event::KeyDown { keycode: Some(Keycode::Num1), .. } => {
-                    self.key_1_down_ = true;
-                }
-                Event::KeyUp { keycode: Some(Keycode::Num1), .. } => {
-                    self.key_1_up_ = true;
-                }
-                Event::KeyDown { keycode: Some(Keycode::Num2), .. } => {
-                    self.key_2_down_ = true;
-                }
-                Event::KeyUp { keycode: Some(Keycode::Num2), .. } => {
-                    self.key_2_up_ = true;
-                }
-                Event::KeyDown { keycode: Some(Keycode::Num3), .. } => {
-                    self.key_3_down_ = true;
-                }
-                Event::KeyUp { keycode: Some(Keycode::Num3), .. } => {
-                    self.key_3_up_ = true;
+                Event::KeyUp{keycode: Some(keycode), ..} => {
+                    match keycode {
+                        Keycode::Escape => {
+                            self.events[GMEventCode::KeyESCUp as usize] = true;
+                        }
+                        Keycode::Num0 => {
+                            self.events[GMEventCode::Key0Up as usize] = true;
+                        }
+                        Keycode::Num1 => {
+                            self.events[GMEventCode::Key1Up as usize] = true;
+                        }
+                        Keycode::Num2 => {
+                            self.events[GMEventCode::Key2Up as usize] = true;
+                        }
+                        Keycode::Num3 => {
+                            self.events[GMEventCode::Key3Up as usize] = true;
+                        }
+                        Keycode::Num4 => {
+                            self.events[GMEventCode::Key4Up as usize] = true;
+                        }
+                        Keycode::Num5 => {
+                            self.events[GMEventCode::Key5Up as usize] = true;
+                        }
+                        Keycode::Num6 => {
+                            self.events[GMEventCode::Key6Up as usize] = true;
+                        }
+                        Keycode::Num7 => {
+                            self.events[GMEventCode::Key7Up as usize] = true;
+                        }
+                        Keycode::Num8 => {
+                            self.events[GMEventCode::Key8Up as usize] = true;
+                        }
+                        Keycode::Num9 => {
+                            self.events[GMEventCode::Key9Up as usize] = true;
+                        }
+                        _ => {
+                            // Unsupported keycode for now                            
+                        }
+                    }
                 }
                 _ => {
-
+                    // Unsupported event for now
                 }
             }
         }
-    }
-
-    pub fn key_esc_down(&self) -> bool {
-        self.key_esc_down_
-    }
-
-    pub fn key_esc_up(&self) -> bool {
-        self.key_esc_up_
-    }
-
-    pub fn key_0_down(&self) -> bool {
-        self.key_0_down_
-    }
-
-    pub fn key_0_up(&self) -> bool {
-        self.key_0_up_
-    }
-
-    pub fn key_1_down(&self) -> bool {
-        self.key_1_down_
-    }
-
-    pub fn key_1_up(&self) -> bool {
-        self.key_1_up_
-    }
-
-    pub fn key_2_down(&self) -> bool {
-        self.key_2_down_
-    }
-
-    pub fn key_2_up(&self) -> bool {
-        self.key_2_up_
-    }
-
-    pub fn key_3_down(&self) -> bool {
-        self.key_3_down_
-    }
-
-    pub fn key_3_up(&self) -> bool {
-        self.key_3_up_
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,12 @@ pub mod texture;
 pub mod timer;
 pub mod util;
 
+pub use bitmap_text::{GMBitmapFont, GMBitmapText};
 pub use context::{GMContext};
 pub use engine::GMEngine;
+pub use input::GMEventCode;
+pub use resources::GMResources;
 pub use scene::GMSceneT;
-pub use bitmap_text::{GMBitmapFont, GMBitmapText};
 
 // Export from SDL2
 pub use sdl2::pixels::Color;
