83e2829172a352c35657b3707a72ff9affaaaf93
Willi Kappler <willi.kappler.gm@gmail.com>
2022-08-30 17:31:34 +0200

    Add some effects for text test 3

--- examples/text3/src/main.rs
+++ examples/text3/src/main.rs
@@ -2,15 +2,20 @@
 
 use std::fs::File;
 
-use log::{debug};
+// use log::{debug};
 use simplelog::{WriteLogger, LevelFilter, ConfigBuilder};
 
-use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMBitmapText, GMBitmapFont, GMEventCode, GMResources, GMAlign, GMTextEffectT};
+use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMEventCode};
+use green_moon_2d::bitmap_text::{GMBitmapText, GMTextEffectT, GMTextEffectEmpty, GMTextEffectWave,
+    GMTextEffectShake, GMTextEffectRotateChars};
+use green_moon_2d::util::GMAlign;
+
 
 #[derive(Debug)]
 struct TextScene3 {
     texts: Vec<GMBitmapText>,
     effects: Vec<Box<dyn GMTextEffectT>>,
+    current_effect: usize,
 }
 
 impl TextScene3 {
@@ -30,12 +35,18 @@ impl TextScene3 {
         // Move title to the center of the window
         texts[0].align(GMAlign::TopCenter);
 
-        let mut effects = Vec::new();
+        let mut effects: Vec<Box<dyn GMTextEffectT>> = Vec::new();
 
+        effects.push(Box::new(GMTextEffectEmpty::new()));
+        effects.push(Box::new(GMTextEffectWave::new(32.0, 0.1, 0.2)));
+        effects.push(Box::new(GMTextEffectShake::new(5.0, 0.2)));
+        effects.push(Box::new(GMTextEffectRotateChars::new(1.0, 10.0)));
+        // effects.push(Box::new());
 
         Self {
             texts,
             effects,
+            current_effect: 0,
         }
     }
 }
@@ -49,15 +60,19 @@ impl GMSceneT for TextScene3 {
         }
 
         if context.event(GMEventCode::Key1Up) {
+            self.current_effect = 0;
         }
 
         if context.event(GMEventCode::Key2Up) {
+            self.current_effect = 1;
         }
 
         if context.event(GMEventCode::Key3Up) {
+            self.current_effect = 2;
         }
 
         if context.event(GMEventCode::Key4Up) {
+            self.current_effect = 3;
         }
 
         if context.event(GMEventCode::Key5Up) {
@@ -74,6 +89,17 @@ impl GMSceneT for TextScene3 {
 
         if context.event(GMEventCode::Key9Up) {
         }
+
+        match self.current_effect {
+            0 | 1 | 3 => {
+                self.texts[0].reset_chars2();
+            }
+            _ => {
+            }
+        }
+
+        self.effects[self.current_effect].update(&mut self.texts[0], context);
+
     }
 
     fn draw(&self, context: &mut GMContext) {
@@ -82,6 +108,8 @@ impl GMSceneT for TextScene3 {
         for text in self.texts.iter() {
             text.draw(context);
         }
+
+        self.effects[self.current_effect].draw(&self.texts[0], context);
     }
 }
 
