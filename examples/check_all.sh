66ae6ec8e332fce1d564fc71507cd7af768e6657
Willi Kappler <willi.kappler.gm@gmail.com>
2022-10-05 14:27:05 +0200

    Refactor

--- examples/check_all.sh
+++ examples/check_all.sh
@@ -1,15 +1,6 @@
-cd text1
-cargo check
-cd ..
-
-cd text2
-cargo check
-cd ..
-
-cd text3
-cargo check
-cd ..
-
-cd sprite1
-cargo check
-cd ..
+for folder in text1 text2 text3 sprite1; do
+  echo $folder
+  cd $folder
+  cargo check
+  cd ..
+done
