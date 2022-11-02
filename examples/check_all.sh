ae294065079622e633705802fc82d7c0665cf097
Willi Kappler <willi.kappler.gm@gmail.com>
2022-11-02 23:30:38 +0100

    Refactor folder iteration

--- examples/check_all.sh
+++ examples/check_all.sh
@@ -1,6 +1,8 @@
-for folder in text1 text2 text3 sprite1; do
-  echo $folder
-  cd $folder
-  cargo check
-  cd ..
+for folder in *; do
+  if [[ -d $folder ]]; then
+    echo $folder
+    cd $folder
+    cargo check
+    cd ..
+  fi
 done
--- examples/update_all.sh
+++ examples/update_all.sh
@@ -1,8 +1,10 @@
-for folder in text1 text2 text3 sprite1; do
-  echo $folder
-  cd $folder
-  cargo clean
-  cargo update
-  cargo check
-  cd ..
+for folder in *; do
+  if [[ -d $folder ]]; then
+    echo $folder
+    cd $folder
+    cargo clean
+    cargo update
+    cargo check
+    cd ..
+  fi
 done
