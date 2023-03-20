b621a93a8fd841a9b1db65bc5c5b1b9f23db5275
Willi Kappler <willi.kappler.gm@gmail.com>
2023-03-20 11:06:23 +0100

    Add update2 for context, change flipt trait to have more functions, add toggle for active, add toggle for visible, add GMSizeT

--- src/util.rs
+++ src/util.rs
@@ -4,6 +4,7 @@ use nanorand::{WyRand, Rng};
 use log::{error, debug};
 
 use crate::context::{GMContext};
+use crate::math::{GMSize, GMFlipXY};
 
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub enum GMAlign {
@@ -90,35 +91,62 @@ pub trait GMDrawT {
 }
 
 pub trait GMUpdateT {
-    fn update(&mut self, _context: &mut GMContext) {
+    fn update(&mut self) {
+    }
+
+    fn update2(&mut self, _context: &mut GMContext) {
+        self.update()
     }
 }
 
 pub trait GMFlipXYT {
-    fn set_flip_x(&mut self, flip_x: bool);
-    fn get_flip_x(&self) -> bool;
-    fn set_flip_y(&mut self, flip_y: bool);
-    fn get_flip_y(&self) -> bool;
+    fn set_flip_x(&mut self, flip_x: bool) {
+        self.get_flip_xy_mut().flip_x = flip_x;
+    }
+
+    fn get_flip_x(&self) -> bool {
+        self.get_flip_xy().flip_x
+    }
+
+    fn toggle_x(&mut self) {
+        let flip_x = self.get_flip_x();
+        self.get_flip_xy_mut().flip_x = !flip_x;
+    }
+
+    fn set_flip_y(&mut self, flip_y: bool) {
+        self.get_flip_xy_mut().flip_y = flip_y;
+    }
+
+    fn get_flip_y(&self) -> bool {
+        self.get_flip_xy().flip_y
+    }
+
+    fn set_flip_xy(&mut self, flip_x: bool, flip_y: bool) {
+        let flip_xy = self.get_flip_xy_mut();
+        flip_xy.flip_x = flip_x;
+        flip_xy.flip_y = flip_y;
+    }
+
+    fn toggle_y(&mut self) {
+        let flip_y = self.get_flip_y();
+        self.get_flip_xy_mut().flip_y = !flip_y;
+    }
+
+    fn get_flip_xy(&self) -> &GMFlipXY;
+
+    fn get_flip_xy_mut(&mut self) -> &mut GMFlipXY;
 }
 
 #[macro_export]
 macro_rules! gen_impl_flipxy {
     ($type:ty) => {
         impl GMFlipXYT for $type {
-            fn set_flip_x(&mut self, flip_x: bool) {
-                self.flip_x = flip_x;
+            fn get_flip_xy(&self) -> &GMFlipXY {
+                &self.flip_xy
             }
 
-            fn get_flip_x(&self) -> bool {
-                self.flip_x
-            }
-
-            fn set_flip_y(&mut self, flip_y: bool) {
-                self.flip_y = flip_y;
-            }
-
-            fn get_flip_y(&self) -> bool {
-                self.flip_y
+            fn get_flip_xy_mut(&mut self) -> &mut GMFlipXY {
+                &mut self.flip_xy
             }
         }
     };
@@ -126,7 +154,13 @@ macro_rules! gen_impl_flipxy {
 
 pub trait GMActiveT {
     fn set_active(&mut self, active: bool);
+
     fn get_active(&self) -> bool;
+
+    fn toggle(&mut self) {
+        let active = self.get_active();
+        self.set_active(!active);
+    }
 }
 
 #[macro_export]
@@ -146,7 +180,13 @@ macro_rules! gen_impl_active {
 
 pub trait GMVisibleT {
     fn set_visible(&mut self, visible: bool);
+
     fn get_visible(&self) -> bool;
+
+    fn toggle(&mut self) {
+        let visible = self.get_visible();
+        self.set_visible(!visible);
+    }
 }
 
 #[macro_export]
@@ -159,6 +199,49 @@ macro_rules! gen_impl_visible {
 
             fn get_visible(&self) -> bool {
                 self.visible
+            }
+        }
+    };
+}
+
+pub trait GMSizeT {
+    fn set_width(&mut self, width: f32) {
+        self.get_size_mut().width = width;
+    }
+
+    fn get_width(&self) -> f32 {
+        self.get_size().width
+    }
+
+    fn set_height(&mut self, height: f32) {
+        self.get_size_mut().height = height;
+    }
+
+    fn get_height(&self) -> f32 {
+        self.get_size().height
+    }
+
+    fn set_size(&mut self, width: f32, height: f32) {
+        let size = self.get_size_mut();
+        size.width = width;
+        size.height = height;
+    }
+
+    fn get_size(&self) -> &GMSize;
+
+    fn get_size_mut(&mut self) -> &mut GMSize;
+}
+
+#[macro_export]
+macro_rules! gen_impl_size {
+    ($type:ty) => {
+        impl GMSizeT for $type {
+            fn get_size(&self) -> &GMSize {
+                &self.size
+            }
+
+            fn get_size_mut(&mut self) -> &mut GMSize {
+                &mut self.size
             }
         }
     };
