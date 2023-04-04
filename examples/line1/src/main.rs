8799de40ab6f68d8064ce96689458da7974bb006
Willi Kappler <grandor@gmx.de>
2023-04-04 16:54:03 +0200

    Finish line1 example

--- examples/line1/src/main.rs
+++ examples/line1/src/main.rs
@@ -5,87 +5,50 @@ use simplelog::{WriteLogger, LevelFilter, ConfigBuilder};
 
 use green_moon_2d::{GMEngine, GMSceneT, GMContext, GMEventCode};
 use green_moon_2d::bitmap_text::{GMBitmapText};
-use green_moon_2d::util::{GMDrawT, GMAlign, GMRepetition, GMUpdateT, GMFlipXYT};
+use green_moon_2d::util::{GMDrawT, GMAlign, GMRepetition, GMUpdateT};
 use green_moon_2d::sprite::{GMSprite};
-use green_moon_2d::movement::{GMMV2Points, GMMVRotate, GMMVCircle, GMMVScale};
+use green_moon_2d::movement::{GMMV2Points};
+use green_moon_2d::line::{GMLine, GMLineMode};
 
 #[derive(Debug)]
-struct SpriteScene1 {
+struct LineScene1 {
     title: GMBitmapText,
-    bat_sprite: GMSprite,
-    ghost_sprite: GMSprite,
-    ice1_sprite: GMSprite,
-    ice1_movement: GMMV2Points,
-    ice1_rotation: GMMVRotate,
-    head_sprite: GMSprite,
-    head_circle: GMMVCircle,
-    ice_troll1_sprite: GMSprite,
-    ice_troll1_movement: GMMV2Points,
-    ice_troll1_scale: GMMVScale,
+    move_start: GMMV2Points,
+    move_end: GMMV2Points,
+    line: GMLine,
 }
 
-impl SpriteScene1 {
+impl LineScene1 {
     fn new(engine: &GMEngine) -> Self {
         // Set up title text:
         let resources = engine.get_resources();
         let font = resources.get_font("font_cuddly");
-        let mut title = GMBitmapText::new(&font, (512.0, 100.0), "SPRITE 1");
+        let mut title = GMBitmapText::new(&font, (512.0, 60.0), "LINE 1");
         title.set_align2(GMAlign::BottomCenter);
 
-        // Bat sprite
-        let texture = resources.get_texture("tex_bat1");
-        let animation = resources.get_animation("anim_bat1");
-        let bat_sprite = GMSprite::new(texture, (512.0, 200.0), animation);
+        // Particle sprite
+        let texture = resources.get_texture("tex_particle1");
+        let animation = resources.get_animation("anim_particle1");
+        let particle_sprite = GMSprite::new(texture, (512.0, 200.0), animation);
 
-        // Ghost sprite
-        let texture = resources.get_texture("tex_ghost1");
-        let animation = resources.get_animation("anim_ghost1");
-        let mut ghost_sprite = GMSprite::new(texture, (512.0, 250.0), animation);
-        ghost_sprite.set_flip_x(true);
+        let mut move_start = GMMV2Points::new((20.0, 40.0), (100.0, 700.0), 0.002);
+        move_start.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
 
-        // Ice1 sprite
-        let texture = resources.get_texture("tex_ice_cream1");
-        let animation = resources.get_empty_animation();
-        let ice1_sprite = GMSprite::new(texture, (100.0, 300.0), animation);
+        let mut move_end = GMMV2Points::new((980.0, 40.0), (900.0, 700.0), 0.003);
+        move_end.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
 
-        let mut ice1_movement = GMMV2Points::new((100.0, 300.0), (900.0, 300.0), 0.007);
-        ice1_movement.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
-        let mut ice1_rotation = GMMVRotate::new(-30.0, 30.0, 0.05);
-        ice1_rotation.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
-
-        // Head sprite
-        let texture = resources.get_texture("tex_head1");
-        let animation = resources.get_animation("anim_head1");
-        let head_sprite = GMSprite::new(texture, (512.0, 400.0), animation);
-        let mut head_circle = GMMVCircle::new(90.0-60.0, 90.0+60.0, 0.02, (512.0, 400.0), 70.0);
-        head_circle.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
-
-        // Ice troll1 sprite
-        let texture = resources.get_texture("tex_ice_troll1");
-        let animation = resources.get_animation("anim_ice_troll1");
-        let ice_troll1_sprite = GMSprite::new(texture, (512.0, 600.0), animation);
-        let mut ice_troll1_movement = GMMV2Points::new((100.0, 600.0), (900.0, 600.0), 0.002);
-        ice_troll1_movement.get_interpolation_mut().set_repetition(GMRepetition::LoopForward);
-        let mut ice_troll1_scale = GMMVScale::new(0.5, 4.0, 0.01);
-        ice_troll1_scale.get_interpolation_mut().set_repetition(GMRepetition::PingPongForward);
+        let line = GMLine::new((0.0, 0.0), (0.0, 0.0), particle_sprite, GMLineMode::Spacing(30.0));
 
         Self {
             title,
-            bat_sprite,
-            ghost_sprite,
-            ice1_sprite,
-            ice1_movement,
-            ice1_rotation,
-            head_sprite,
-            head_circle,
-            ice_troll1_sprite,
-            ice_troll1_movement,
-            ice_troll1_scale,
+            move_start,
+            move_end,
+            line,
         }
     }
 }
 
-impl GMSceneT for SpriteScene1 {
+impl GMSceneT for LineScene1 {
     fn update(&mut self, context: &mut GMContext) {
         if context.event(GMEventCode::KeyESCUp) ||
            context.event(GMEventCode::Quit) ||
@@ -93,41 +56,30 @@ impl GMSceneT for SpriteScene1 {
             context.quit();
         }
 
-        self.bat_sprite.update();
-        self.ghost_sprite.update();
-
-        self.ice1_movement.set_and_update(&mut self.ice1_sprite);
-        self.ice1_rotation.set_and_update(&mut self.ice1_sprite);
-
-        self.head_sprite.update();
-        self.head_circle.set_and_update(&mut self.head_sprite);
-
-        self.ice_troll1_sprite.update();
-        self.ice_troll1_movement.set_and_update(&mut self.ice_troll1_sprite);
-        self.ice_troll1_scale.set_and_update(&mut self.ice_troll1_sprite);
+        self.move_start.set_and_update_n(&mut self.line, 0);
+        self.move_end.set_and_update_n(&mut self.line, 1);
+        self.line.end_point_changed();
+        self.line.update();
     }
 
     fn draw(&self, context: &mut GMContext) {
         context.clear_black();
 
         self.title.draw(context);
-        self.bat_sprite.draw(context);
-        self.ghost_sprite.draw(context);
-        self.ice1_sprite.draw(context);
-        self.head_sprite.draw(context);
-        self.ice_troll1_sprite.draw(context);
+
+        self.line.draw(context);
     }
 }
 
 fn main() {
     let log_config = ConfigBuilder::new().build();
-    let _simple_log = WriteLogger::init(LevelFilter::Debug, log_config, File::create("sprite1.log").expect("Could not create log file"));
+    let _simple_log = WriteLogger::init(LevelFilter::Debug, log_config, File::create("line1.log").expect("Could not create log file"));
 
     let mut engine = GMEngine::new();
     engine.load_configuration_and_init("config.json");
 
-    let sprite1_scene = SpriteScene1::new(&engine);
+    let line1_scene = LineScene1::new(&engine);
 
-    engine.add_scene("sprite1_scene", sprite1_scene);
+    engine.add_scene("line1_scene", line1_scene);
     engine.run();
 }
