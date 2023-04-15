134fac917e23a2f169bb10658987e0a13f02ee97
Willi Kappler <willi.kappler.gm@gmail.com>
2023-04-15 19:13:55 +0200

    Use try_borrow(), add multi methods for GMObjectT trait

--- src/object.rs
+++ src/object.rs
@@ -59,28 +59,33 @@ impl GMObjectManager {
 
     pub fn remove_object(&mut self, name: &str) {
         self.objects.retain(|o| {
-            let object = o.borrow(); // TODO: use try borrow
-            let maybe_name = object.get_property(GMProperty::Name);
+            if let Ok(object) = o.try_borrow() {
+                let maybe_name = object.get_property(GMProperty::Name);
 
-            if let Some(GMValue::String(name2)) = maybe_name {
-                name2 != name
+                if let Some(GMValue::String(name2)) = maybe_name {
+                    name2 != name
+                } else {
+                    true
+                }
             } else {
-                true
+                false
             }
         });
     }
 
     pub fn update(&self, context: &mut GMContext) {
         for object in self.objects.iter() {
-            let mut object = object.borrow_mut(); // TODO: use try borrow
-            object.send_message(GMMessage::Update, context, &self);
+            if let Ok(mut object) = object.try_borrow_mut() {
+                object.send_message(GMMessage::Update, context, &self);
+            }
         }
     }
 
     pub fn draw(&self, context: &mut GMContext) {
         for object in self.objects.iter() {
-            let mut object = object.borrow_mut(); // TODO: use try borrow
-            object.send_message(GMMessage::Draw, context, &self);
+            if let Ok(mut object) = object.try_borrow_mut() {
+                object.send_message(GMMessage::Draw, context, &self);
+            }
         }
     }
 
@@ -91,12 +96,13 @@ impl GMObjectManager {
 
     pub fn send_message(&self, name: &str, message: GMMessage, context: &mut GMContext) -> Option<GMValue> {
         for object in self.objects.iter() {
-            let mut object = object.borrow_mut(); // TODO: use try borrow
-            let maybe_name = object.get_property(GMProperty::Name);
+            if let Ok(mut object) = object.try_borrow_mut() {
+                let maybe_name = object.get_property(GMProperty::Name);
 
-            if let Some(GMValue::String(name2)) = maybe_name {
-                if name2 == name {
-                    return object.send_message(message, context, &self)
+                if let Some(GMValue::String(name2)) = maybe_name {
+                    if name2 == name {
+                        return object.send_message(message, context, &self)
+                    }
                 }
             }
         }
@@ -104,17 +110,19 @@ impl GMObjectManager {
         None
     }
 
-    pub fn send_message_group(&self, group: &str, message: GMMessage, context: &mut GMContext) -> Vec<Option<GMValue>> {
+    pub fn send_message_group(&self, group: &str, message: GMMessage, context: &mut GMContext) -> Vec<GMValue> {
         let mut result = Vec::new();
 
         for object in self.objects.iter() {
-            let mut object = object.borrow_mut(); // TODO: use try borrow
-            let maybe_in_group = object.send_message(GMMessage::InGroup(group.to_string()), context, &self);
+            if let Ok(mut object) = object.try_borrow_mut() {
+                let maybe_in_group = object.send_message(GMMessage::InGroup(group.to_string()), context, &self);
 
-            if let Some(GMValue::Bool(in_group)) = maybe_in_group {
-                if in_group {
-                    let value = object.send_message(message.clone(), context, &self);
-                    result.push(value);
+                if let Some(GMValue::Bool(in_group)) = maybe_in_group {
+                    if in_group {
+                        if let Some(value) = object.send_message(message.clone(), context, &self) {
+                            result.push(value);
+                        }
+                    }
                 }
             }
         }
@@ -124,13 +132,14 @@ impl GMObjectManager {
 
     pub fn set_property(&self, name: &str, property: GMProperty, value: GMValue) {
         for object in self.objects.iter() {
-            let mut object = object.borrow_mut(); // TODO: use try borrow
-            let maybe_name = object.get_property(GMProperty::Name);
+            if let Ok(mut object) = object.try_borrow_mut() {
+                let maybe_name = object.get_property(GMProperty::Name);
 
-            if let Some(GMValue::String(name2)) = maybe_name {
-                if name2 == name {
-                    object.set_property(property, value);
-                    break;
+                if let Some(GMValue::String(name2)) = maybe_name {
+                    if name2 == name {
+                        object.set_property(property, value);
+                        break;
+                    }
                 }
             }
         }
@@ -138,12 +147,13 @@ impl GMObjectManager {
 
     pub fn get_property(&self, name: &str, property: GMProperty) -> Option<GMValue> {
         for object in self.objects.iter() {
-            let object = object.borrow(); // TODO: use try borrow
-            let maybe_name = object.get_property(GMProperty::Name);
+            if let Ok(object) = object.try_borrow() {
+                let maybe_name = object.get_property(GMProperty::Name);
 
-            if let Some(GMValue::String(name2)) = maybe_name {
-                if name2 == name {
-                    return object.get_property(property)
+                if let Some(GMValue::String(name2)) = maybe_name {
+                    if name2 == name {
+                        return object.get_property(property)
+                    }
                 }
             }
         }
@@ -157,12 +167,24 @@ impl GMObjectManager {
 // TODO: Add pre-processing for messages: position, active, visible, ...
 
 pub trait GMObjectT {
-    fn send_message(&mut self, message: GMMessage, context: &mut GMContext, object_manager: &GMObjectManager) -> Option<GMValue>;
-    // TODO: send multiple messages
+    fn send_multi_message(&mut self, messages: Vec<GMMessage>, context: &mut GMContext, object_manager: &GMObjectManager) -> Vec<GMValue>;
 
-    fn set_property(&mut self, property: GMProperty, value: GMValue);
-    // TODO: set multiple properties
+    fn send_message(&mut self, message: GMMessage, context: &mut GMContext, object_manager: &GMObjectManager) -> Option<GMValue> {
+        let mut result = self.send_multi_message(vec![message], context, object_manager);
+        result.pop()
+    }
 
-    fn get_property(&self, property: GMProperty) -> Option<GMValue>;
-    // TODO: get multiple properties
+    fn set_multi_property(&mut self, properties: Vec<(GMProperty, GMValue)>);
+
+    fn set_property(&mut self, property: GMProperty, value: GMValue) {
+        self.set_multi_property(vec![(property, value)])
+    }
+
+    fn get_multi_property(&self, properties: Vec<GMProperty>) -> Vec<GMValue>;
+
+    fn get_property(&self, property: GMProperty) -> Option<GMValue> {
+        let mut result = self.get_multi_property(vec![property]);
+        result.pop()
+    }
+
 }
