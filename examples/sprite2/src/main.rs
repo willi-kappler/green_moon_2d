d7041a323117afe193259bde91210a4eccfe160e
Willi Kappler <willi.kappler.gm@gmail.com>
2023-05-17 21:58:55 +0200

    Add more code for sprite 2 example

--- examples/sprite2/src/main.rs
+++ examples/sprite2/src/main.rs
@@ -9,7 +9,8 @@ use green_moon_2d::object_manager::GMObjectManager;
 use green_moon_2d::sprite::GMSprite;
 use green_moon_2d::object_util::{GMValueInterpolateF32, GMValueInterpolateVec2D};
 use green_moon_2d::message::GMMessage;
-use green_moon_2d::movement::GMMVCircle;
+use green_moon_2d::movement::{GMMVCircle, GMMVMultiCircle};
+use green_moon_2d::target::GMTarget;
 
 
 #[derive(Debug)]
@@ -33,89 +34,79 @@ impl SpriteScene2 {
         text.set_align(GMAlign::BottomCenter);
         object_manager.add_draw_object("title", text, 0, 0);
 
-        // Set up sprites:
+        // Set up objects:
+        // Set up text
+        text = GMBitmapText::new(font1, (0.0, 0.0), "BOOO!");
+        text.set_align(GMAlign::MiddleCenter);
+        object_manager.add_draw_object("ghost_text1", text, 0, 0);
 
-        // Bat sprite
-        let texture = resources.get_texture("tex_bat1");
-        let animation = resources.get_animation("anim_bat1");
-        let mut sprite = GMSprite::new((512.0, 200.0), texture, animation);
-        object_manager.add_draw_object("bat1", sprite, 0, 0);
-
-        // Ghost sprite
+        // Set up ghost sprite
         let texture = resources.get_texture("tex_ghost1");
         let animation = resources.get_animation("anim_ghost1");
-        sprite = GMSprite::new((512.0, 250.0), texture, animation);
-        sprite.flip_x = true;
-        object_manager.add_draw_object("ghost1", sprite, 0, 0);
+        let sprite = GMSprite::new((0.0, 0.0), texture, animation.clone());
+        object_manager.add_draw_object("ghost_sprite1", sprite.clone(), 0, 1);
 
+        // Big outer circle
+        let target: GMTarget = ("ghost_sprite1", "small_circle").into();
+        let mut circle = GMMVCircle::new(target, (250.0, 250.0), 100.0);
+        object_manager.add_normal_object("big_circle", circle, 0);
 
-        // Ice1 sprite
-        let texture = resources.get_texture("tex_ice_cream1");
-        let animation = resources.get_empty_animation();
-        sprite = GMSprite::new((100.0, 300.0), texture, animation);
-        object_manager.add_draw_object("ice1", sprite, 0, 0);
-
-        let mut interpolate = GMValueInterpolateVec2D::new((100.0, 300.0), (900.0, 300.0), 0.007,
+        let mut interpolate = GMValueInterpolateF32::new(0.0, 360.0, 0.001,
             |value, context, object_manager| {
-                let target = "ice1".into();
-                let message = GMMessage::SetPosition(value);
-                object_manager.send_message(&target, message, context);
-            }
-        );
-        interpolate.interpolation.repetition = GMRepetition::PingPongForward;
-        object_manager.add_normal_object("move_ice1", interpolate, 0);
-
-        let mut interpolate = GMValueInterpolateF32::new(-30.0, 30.0, 0.05,
-            |value, context, object_manager| {
-                let target = "ice1".into();
+                let target = "big_circle".into();
                 object_manager.send_custom_message1(&target, "set_angle", value, context);
-            }
-        );
-        interpolate.interpolation.repetition = GMRepetition::PingPongForward;
-        object_manager.add_normal_object("rotate_ice1", interpolate, 0);
-
-        // Head sprite
-        let texture = resources.get_texture("tex_head1");
-        let animation = resources.get_animation("anim_head1");
-        sprite = GMSprite::new((512.0, 400.0), texture, animation);
-        object_manager.add_draw_object("head1", sprite, 0, 0);
-
-        let circle = GMMVCircle::new("head1", (512.0, 400.0).into(), 70.0);
-        object_manager.add_normal_object("circle_head1", circle, 0);
-
-        let mut interpolate = GMValueInterpolateF32::new(90.0-60.0, 90.0+60.0, 0.02,
-            |value, context, object_manager| {
-                let target = "circle_head1".into();
-                object_manager.send_custom_message1(&target, "set_angle", value, context);
-            }
-        );
-        interpolate.interpolation.repetition = GMRepetition::PingPongForward;
-        object_manager.add_normal_object("angle_circle_head1", interpolate, 0);
-
-        // Ice troll1 sprite
-        let texture = resources.get_texture("tex_ice_troll1");
-        let animation = resources.get_animation("anim_ice_troll1");
-        sprite = GMSprite::new((512.0, 600.0), texture, animation);
-        object_manager.add_draw_object("ice_troll1", sprite, 0, 0);
-
-        let mut interpolate = GMValueInterpolateVec2D::new((100.0, 600.0), (900.0, 600.0), 0.002,
-            |value, context, object_manager| {
-                let target = "ice_troll1".into();
-                let message = GMMessage::SetPosition(value);
-                object_manager.send_message(&target, message, context);
             }
         );
         interpolate.interpolation.repetition = GMRepetition::LoopForward;
-        object_manager.add_normal_object("move_ice_troll1", interpolate, 0);
+        object_manager.add_normal_object("big_angle", interpolate, 0);
 
-        let mut interpolate = GMValueInterpolateF32::new(0.5, 4.0, 0.01,
+        // Small inner circle
+        circle = GMMVCircle::new("ghost_text1", (0.0, 0.0), 50.0);
+        object_manager.add_normal_object("small_circle", circle, 0);
+
+        let mut interpolate = GMValueInterpolateF32::new(0.0, 360.0, 0.01,
             |value, context, object_manager| {
-                let target = "ice_troll1".into();
-                object_manager.send_custom_message1(&target, "set_scale", value, context);
+                let target = "small_circle".into();
+                object_manager.send_custom_message1(&target, "set_angle", value, context);
+            }
+        );
+        interpolate.interpolation.repetition = GMRepetition::LoopForward;
+        object_manager.add_normal_object("small_angle", interpolate, 0);
+
+        // 4 ghosts in a circle
+        object_manager.add_draw_object("multi_sprite1", sprite.clone(), 0, 0);
+        object_manager.add_draw_object("multi_sprite2", sprite.clone(), 0, 0);
+        object_manager.add_draw_object("multi_sprite3", sprite.clone(), 0, 0);
+        object_manager.add_draw_object("multi_sprite4", sprite.clone(), 0, 0);
+
+        let multi_circle = GMMVMultiCircle::new( (600.0, 250.0), 100.0, 90.0, 4,
+            |positions, context, object_manager| {
+                object_manager.send_message_object("multi_sprite1", GMMessage::SetPosition(positions[0]), context);
+                object_manager.send_message_object("multi_sprite2", GMMessage::SetPosition(positions[1]), context);
+                object_manager.send_message_object("multi_sprite3", GMMessage::SetPosition(positions[2]), context);
+                object_manager.send_message_object("multi_sprite4", GMMessage::SetPosition(positions[3]), context);
+            }
+        );
+        object_manager.add_normal_object("multi_circle", multi_circle, 0);
+
+        let mut interpolate = GMValueInterpolateF32::new(0.0, 360.0, 0.005,
+            |value, context, object_manager| {
+                let target = "multi_circle".into();
+                object_manager.send_custom_message1(&target, "set_angle", value, context);
+            }
+        );
+        interpolate.interpolation.repetition = GMRepetition::LoopForward;
+        object_manager.add_normal_object("multi_angle", interpolate, 0);
+
+        let mut interpolate = GMValueInterpolateF32::new(50.0, 100.0, 0.02,
+            |value, context, object_manager| {
+                let target = "multi_circle".into();
+                object_manager.send_custom_message1(&target, "set_radius", value, context);
             }
         );
         interpolate.interpolation.repetition = GMRepetition::PingPongForward;
-        object_manager.add_normal_object("scale_ice_troll1", interpolate, 0);
+        object_manager.add_normal_object("multi_radius", interpolate, 0);
+
 
         Self {
             object_manager,
