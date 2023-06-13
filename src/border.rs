04c319899f44f3abac2338d5c1c02ded92380d23
Willi Kappler <grandor@gmx.de>
2023-06-13 18:46:05 +0200

    Refactor

--- src/border.rs
+++ src/border.rs
@@ -5,30 +5,16 @@ use crate::message::GMMessage;
 use crate::value::GMValue;
 use crate::object_manager::GMObjectManager;
 use crate::context::GMContext;
-use crate::math::GMVec2D;
-
-pub enum GMBorderTiles {
-    Tile1(Box<dyn GMObjectT>),
-    Tile4,
-    Tile9,
-}
+use crate::math::{GMVec2D, GMRectangle};
 
 #[derive(Debug, Clone)]
-pub struct GMBorder {
-    top_left: GMVec2D,
-    width: u32,
-    height: u32,
+pub struct GMBorderBase {
+    pub rectangle: GMRectangle,
 }
 
-impl GMObjectT for GMBorder {
+impl GMObjectT for GMBorderBase {
     fn send_message(&mut self, message: GMMessage, object_manager: &GMObjectManager) -> GMValue {
         GMValue::None
-    }
-
-    fn update(&mut self, object_manager: &GMObjectManager, context: &mut GMContext) {
-    }
-
-    fn draw(&self, context: &mut GMContext) {
     }
 
     fn clone_box(&self) -> Box<dyn GMObjectT> {
