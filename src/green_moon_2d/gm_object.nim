e520d152a7fa7294e53577fa0e4078508041792e
Willi Kappler <willi.kappler.gm@gmail.com>
2024-04-28 18:41:22 +0200

    Add some more code for objects and object handling

--- src/green_moon_2d/gm_object.nim
+++ src/green_moon_2d/gm_object.nim
@@ -9,57 +9,119 @@
 
 
 # Nim std imports
+import std/json
 import std/sets
+import std/options
+import std/algorithm
+
+from std/strformat import fmt
+
+# Local imports
+import gm_log
 
 type
     GMObject* = ref object of RootObj
         name: string
-        groups: HashSet[string]
-        order: int32
-
-    GMGFXObject* = ref object of RootObj
-        name: string
-        groups: HashSet[string]
-        order: int32
-        x: float32
-        y: float32
+        groups*: HashSet[string]
+        updateOrder*: int32
+        drawOrder*: int32
+        active*: bool
+        visible*: bool
+        x*: float32
+        y*: float32
 
     GMObjectManager = object
         objects: seq[GMObject]
 
-    GMGFXObjectManager = object
-        objects: seq[GMObject]
+    GMObjectNotFoundError* = object of CatchableError
+    GMObjectAlreadyExistsError* = object of CatchableError
 
 var GMGlobObjects: GMObjectManager
-var GMGlobGFXObjects: GMGFXObjectManager
-
-# GMGFXObject:
-proc gmDraw*(self: var GMGFXObject) =
-    discard
-
-proc gmUpdate*(self: var GMGFXObject) =
-    discard
 
 # GMObject:
-proc gmUpdate*(self: var GMObject) =
+method gmDraw*(self: var GMObject) {.base.} =
+    ## This method can be implemented in order to draw the object.
     discard
 
-# GMGFXObjectManager:
-proc gmInitObjectManager*() =
-    GMGlobObjects = GMObjectManager(objects: @[])
-
-proc gmDrawGFXObjects*() =
+method gmUpdate*(self: var GMObject) {.base.} =
+    ## This method can be implemented in order to update the object.
     discard
 
-proc gmUpdateGFXObjects*() =
-    discard
+method gmSendMessage*(self: var GMObject, message: JsonNode): JsonNode {.base.} =
+    ## This method can be implemented in order to send a custom message to the object.
+    return newJNull()
+
+proc gmFindObject*(name: string): Option[GMObject] =
+    ## Return the object (by name) or none if no such object was found.
+    for o in GMGlobObjects.objects.mitems():
+        if o.name == name:
+            return some(o)
+
+    return none(GMObject)
 
 # GMObjectManager:
-proc gmInitGFXObjectManager*() =
-    GMGlobGFXObjects = GMGFXObjectManager(objects: @[])
+proc gmInitObjectManager*() =
+    ## Initializes the object manager
+    GMGlobObjects = GMObjectManager(objects: @[])
+
+proc gmAddObject*(name: string, newObject: GMObject) =
+    ## Adds a new object by the given name. Raise an exception if that name is already in use.
+    let ob = gmFindObject(name)
+
+    if ob.isSome():
+        error_log(fmt("Can't add object {name}, it already exists!"), GMObjectAlreadyExistsError)
+    else:
+        GMGlobObjects.objects.add(newObject)
+
+proc gmDeleteObject*(name: string) =
+    ## Deletes an object with the given name. Raise an exception if the object was not found.
+    var idx = none(uint32)
+
+    for (i, ob) in GMGlobObjects.objects.pairs():
+        if ob.name == name:
+            idx = some(uint32(i))
+            break
+
+    if idx.isSome():
+        let i = idx.get()
+        GMGlobObjects.objects.del(i)
+    else:
+        error_log(fmt("Can't remove object {name}, not found!"), GMObjectNotFoundError)
+
+proc gmObjectSendMessage*(name: string, message: JsonNode): JsonNode =
+    ## Sends a message to the given object (by name). Raise an exception if that name was not found.
+    for ob in GMGlobObjects.objects.mitems():
+        if ob.name == name:
+            return ob.gmSendMessage(message)
+
+    error_log(fmt("Can't send message to object {name}, not found!"), GMObjectNotFoundError)
+
+proc gmObjectSendMessageGroup*(name: string, message: JsonNode): seq[(string, JsonNode)] =
+    ## Send a message to all objects that belong to the given group.
+    result = @[]
+
+    for ob in GMGlobObjects.objects.mitems():
+        if name in ob.groups:
+            let v = ob.gmSendMessage(message)
+            result.add((ob.name, v))
+
+proc gmDrawObjects*() =
+    ## Calls the draw() method for all visible objects, at each frame.
+    GMGlobObjects.objects.sort do(a, b: GMObject) -> int:
+        return cmp(a.drawOrder, b.drawOrder)
+
+    for o in GMGlobObjects.objects.mitems():
+        if o.visible:
+            o.gmDraw()
 
 proc gmUpdateObjects*() =
-    discard
+    ## Calls update() for all active (non-gfx) objects, at each frame.
+    GMGlobObjects.objects.sort do(a, b: GMObject) -> int:
+        return cmp(a.updateOrder, b.updateOrder)
+
+    for o in GMGlobObjects.objects.mitems():
+        if o.active:
+            o.gmUpdate()
 
 
 
