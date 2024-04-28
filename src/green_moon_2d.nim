f58c4ea47c5775adea30c6231fae3813a4e68cf2
Willi Kappler <willi.kappler.gm@gmail.com>
2024-04-28 18:39:37 +0200

    Add missing log module

--- src/green_moon_2d.nim
+++ src/green_moon_2d.nim
@@ -13,6 +13,7 @@ import green_moon_2d/gm_border
 import green_moon_2d/gm_configuration
 import green_moon_2d/gm_font
 import green_moon_2d/gm_init
+import green_moon_2d/gm_log
 import green_moon_2d/gm_menu
 import green_moon_2d/gm_menuitem
 import green_moon_2d/gm_object
@@ -33,6 +34,7 @@ export gm_border
 export gm_configuration
 export gm_font
 export gm_init
+export gm_log
 export gm_menu
 export gm_menuitem
 export gm_object
