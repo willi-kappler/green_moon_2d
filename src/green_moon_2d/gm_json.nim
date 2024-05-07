e87e02de36cd08e2bc14673b5e966609d0572b5d
Willi Kappler <willi.kappler.gm@gmail.com>
2024-05-07 21:41:55 +0200

    Add some common json utility functions

---
+++ src/green_moon_2d/gm_json.nim
@@ -0,0 +1,45 @@
+## This module is part of GreenMoon2D: https://github.com/willi-kappler/green_moon_2d
+##
+## Written by Willi Kappler, License: MIT
+##
+## This Nim library allows you to write 2D games, it used Naylib (Raylib) internally.
+##
+## This file contains utility function for JSON value extraction
+##
+
+# Nim std imports
+import std/json
+import std/sets
+
+proc gmGetBool*(node: JsonNode, name: string, default: bool = false): bool =
+    return node{name}.getBool(default)
+
+proc gmGetUint8*(node: JsonNode, name: string, default: uint8 = 0): uint8 =
+    return uint8(node{name}.getInt(int(default)))
+
+proc gmGetInt32*(node: JsonNode, name: string, default: int32 = 0): int32 =
+    return int32(node{name}.getInt(int(default)))
+
+proc gmGetUint32*(node: JsonNode, name: string, default: uint32 = 0): uint32 =
+    return uint32(node{name}.getInt(int(default)))
+
+proc gmGetFloat32*(node: JsonNode, name: string, default: float32 = 0.0): float32 =
+    return float32(node{name}.getFloat(float32(default)))
+
+proc gmGetString*(node: JsonNode, name: string, default: string = ""): string =
+    return node{name}.getStr(default)
+
+proc gmGetHashSetString*(node: JsonNode, name: string, default: HashSet[string] = initHashSet[string]()): HashSet[string] =
+    result = default
+    # TODO: implement
+
+
+proc gmGetNode*(node: JsonNode, name: string, default: JsonNode = newJNull()): JsonNode =
+    result = default
+    if node.contains(name):
+        result = node[name]
+
+proc gmGetNodes*(node: JsonNode, name: string, default: seq[JsonNode] = @[]): seq[JsonNode] =
+    result = default
+    # TODO: implement
+
