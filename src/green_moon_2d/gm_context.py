44fd2ce2e12071c9ebe2457de0c3e3e708eba7b4
Willi Kappler <willi.kappler.gm@gmail.com>
2024-06-29 14:21:33 +0200

    Add first code for more modules

---
+++ src/green_moon_2d/gm_context.py
@@ -0,0 +1,21 @@
+# This file is part of GreenMoon2D, a 2D game engine for Python
+# written by Willi Kappler
+#
+# See: https://github.com/willi-kappler/green_moon_2d
+
+"""
+This module defines the GMContext class which is passed to all the update() and draw() methods.
+"""
+
+class GMContext:
+    """
+    This class contains acess to the scene manager and the state of the game engine.
+    It also allows you to store data that is used across multiple scenes.
+    """
+    def __init__(self):
+        game_property = {}
+        pass
+
+
+
+
