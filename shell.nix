8390b9e68943cc6dd42c55332f2de30e22c66f17
Willi Kappler <willi.kappler.gm@gmail.com>
2024-06-29 14:20:58 +0200

    Add nix shell dev environment

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+      (python312.withPackages(ps: with ps; [
+        flake8
+        ipython
+        mypy
+        raylib
+        ]))
+      ruff
+    ];
+}
