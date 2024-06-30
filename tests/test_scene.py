b837ea3fdc91d7a841c8c696e5dd2d764f63037e
Willi Kappler <willi.kappler.gm@gmail.com>
2024-06-30 18:38:00 +0200

    Add more comments and test cased.

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -58,7 +58,7 @@ class TestSceneManager(unittest.TestCase):
 
     def test_add_scene3(self):
         """
-        Test adding a scene with the same name.
+        Test adding a scene with the same name (replace).
         """
         new_scene = TestScene("test1")
         new_scene.custom_property["replaced"] = "No"
@@ -71,11 +71,17 @@ class TestSceneManager(unittest.TestCase):
         self.assertEqual(self.sm.scenes["test1"].custom_property["replaced"], "Yes")
 
     def test_delete_scene1(self):
+        """
+        Test delete scene.
+        """
         self.sm.add_scene(TestScene("test1"))
         self.sm.delete_scene("test1")
         self.assertEqual(len(self.sm.scenes), 0)
 
     def test_delete_scene2(self):
+        """
+        Test delete scene where name is not found.
+        """
         self.sm.add_scene(TestScene("test1"))
 
         with self.assertRaises(KeyError):
@@ -83,6 +89,81 @@ class TestSceneManager(unittest.TestCase):
 
         self.assertEqual(len(self.sm.scenes), 1)
         self.assertIn("test1", self.sm.scenes)
+
+    def test_change_to_scene1(self):
+        """
+        Test change to scene.
+        """
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.add_scene(TestScene("test3"))
+
+        self.sm.start_scene("test2")
+        self.assertEqual(self.sm.current_scene.name, "test2")
+        self.assertEqual(self.sm.current_scene.enter_called, 1)
+        self.assertEqual(self.sm.current_scene.leave_called, 0)
+        self.assertEqual(self.sm.scenes["test1"].enter_called, 0)
+        self.assertEqual(self.sm.scenes["test1"].leave_called, 0)
+        self.assertEqual(self.sm.scenes["test3"].enter_called, 0)
+        self.assertEqual(self.sm.scenes["test3"].leave_called, 0)
+
+        self.sm.change_to_scene("test3")
+        self.assertEqual(self.sm.current_scene.name, "test3")
+        self.assertEqual(self.sm.current_scene.enter_called, 1)
+        self.assertEqual(self.sm.current_scene.leave_called, 0)
+        self.assertEqual(self.sm.scenes["test1"].enter_called, 0)
+        self.assertEqual(self.sm.scenes["test1"].leave_called, 0)
+        self.assertEqual(self.sm.scenes["test2"].enter_called, 1)
+        self.assertEqual(self.sm.scenes["test2"].leave_called, 1)
+
+    def test_change_to_scene1(self):
+        """
+        Test change to scene where name is not found.
+        """
+        with self.assertRaises(KeyError):
+            self.sm.change_to_scene("test1")
+
+    def test_push_and_change1(self):
+        """
+        Test push and change scene.
+        """
+        pass
+
+    def test_push_and_change2(self):
+        """
+        Test push and change scene where name is not found.
+        """
+        pass
+
+    def test_pop_and_change1(self):
+        """
+        Test pop and change scene.
+        """
+        pass
+
+    def test_pop_and_change2(self):
+        """
+        Test pop and change scene where the scene stack is empty.
+        """
+        pass
+
+    def test_start_scene(self):
+        """
+        Test setting the start scene.
+        """
+        pass
+
+    def test_update(self):
+        """
+        Test calling update on the current scene.
+        """
+        pass
+
+    def test_update(self):
+        """
+        Test calling draw on the current scene.
+        """
+        pass
 
 
 if __name__ == '__main__':
