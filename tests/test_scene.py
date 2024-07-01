a9def7d3dbee8472748d593dbcf30c40beccee64
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-01 18:09:38 +0200

    Add helper method for testing, finish test cases.

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -30,6 +30,27 @@ class TestScene(GMScene):
         self.leave_called += 1
 
 class TestSceneManager(unittest.TestCase):
+    def assertSceneProperty(self, name: str, property: str, value):
+        self.assertEqual(self.sm.scenes[name].custom_property[property], value)
+
+    def assertSceneCurrent(self, name: str):
+        self.assertEqual(self.sm.current_scene.name, name)
+
+    def assertSceneEnter(self, name: str, val: int):
+        self.assertEqual(self.sm.scenes[name].enter_called, val)
+
+    def assertSceneLeave(self, name: str, val: int):
+        self.assertEqual(self.sm.scenes[name].leave_called, val)
+
+    def assertSceneUpdate(self, name: str, val: int):
+        self.assertEqual(self.sm.scenes[name].update_called, val)
+
+    def assertSceneDraw(self, name: str, val: int):
+        self.assertEqual(self.sm.scenes[name].draw_called, val)
+
+    def assertSceneStack(self, index: int, name: str):
+        self.assertEqual(self.sm.scene_stack[index].name, name)
+
     def setUp(self):
         self.sm = GMSceneManager()
 
@@ -63,12 +84,12 @@ class TestSceneManager(unittest.TestCase):
         new_scene = TestScene("test1")
         new_scene.custom_property["replaced"] = "No"
         self.sm.add_scene(new_scene)
-        self.assertEqual(self.sm.scenes["test1"].custom_property["replaced"], "No")
+        self.assertSceneProperty("test1", "replaced", "No")
 
         new_scene = TestScene("test1")
         new_scene.custom_property["replaced"] = "Yes"
         self.sm.add_scene(new_scene)
-        self.assertEqual(self.sm.scenes["test1"].custom_property["replaced"], "Yes")
+        self.assertSceneProperty("test1", "replaced", "Yes")
 
     def test_delete_scene1(self):
         """
@@ -99,72 +120,141 @@ class TestSceneManager(unittest.TestCase):
         self.sm.add_scene(TestScene("test3"))
 
         self.sm.start_scene("test2")
-        self.assertEqual(self.sm.current_scene.name, "test2")
-        self.assertEqual(self.sm.current_scene.enter_called, 1)
-        self.assertEqual(self.sm.current_scene.leave_called, 0)
-        self.assertEqual(self.sm.scenes["test1"].enter_called, 0)
-        self.assertEqual(self.sm.scenes["test1"].leave_called, 0)
-        self.assertEqual(self.sm.scenes["test3"].enter_called, 0)
-        self.assertEqual(self.sm.scenes["test3"].leave_called, 0)
+        self.assertSceneCurrent("test2")
+        self.assertSceneEnter("test1", 0)
+        self.assertSceneLeave("test1", 0)
+        self.assertSceneEnter("test2", 1)
+        self.assertSceneLeave("test2", 0)
+        self.assertSceneEnter("test3", 0)
+        self.assertSceneLeave("test3", 0)
 
         self.sm.change_to_scene("test3")
-        self.assertEqual(self.sm.current_scene.name, "test3")
-        self.assertEqual(self.sm.current_scene.enter_called, 1)
-        self.assertEqual(self.sm.current_scene.leave_called, 0)
-        self.assertEqual(self.sm.scenes["test1"].enter_called, 0)
-        self.assertEqual(self.sm.scenes["test1"].leave_called, 0)
-        self.assertEqual(self.sm.scenes["test2"].enter_called, 1)
-        self.assertEqual(self.sm.scenes["test2"].leave_called, 1)
+        self.assertSceneCurrent("test3")
+        self.assertSceneEnter("test1", 0)
+        self.assertSceneLeave("test1", 0)
+        self.assertSceneEnter("test2", 1)
+        self.assertSceneLeave("test2", 1)
+        self.assertSceneEnter("test3", 1)
+        self.assertSceneLeave("test3", 0)
 
-    def test_change_to_scene1(self):
+    def test_change_to_scene2(self):
         """
         Test change to scene where name is not found.
         """
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.start_scene("test1")
+
         with self.assertRaises(KeyError):
-            self.sm.change_to_scene("test1")
+            self.sm.change_to_scene("test2")
 
     def test_push_and_change1(self):
         """
         Test push and change scene.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.add_scene(TestScene("test3"))
+
+        self.assertEqual(len(self.sm.scene_stack), 0)
+
+        self.sm.start_scene("test1")
+        self.sm.push_and_change("test2")
+        self.assertSceneEnter("test1", 1)
+        self.assertSceneLeave("test1", 1)
+        self.assertSceneEnter("test2", 1)
+        self.assertSceneLeave("test2", 0)
+        self.assertSceneCurrent("test2")
+        self.assertEqual(len(self.sm.scene_stack), 1)
+        self.assertSceneStack(0, "test1")
 
     def test_push_and_change2(self):
         """
         Test push and change scene where name is not found.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.add_scene(TestScene("test3"))
+        self.sm.start_scene("test1")
+
+        with self.assertRaises(KeyError):
+            self.sm.push_and_change("test5")
 
     def test_pop_and_change1(self):
         """
         Test pop and change scene.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.add_scene(TestScene("test3"))
+        self.sm.start_scene("test1")
+        self.sm.push_and_change("test2")
+
+        self.sm.pop_and_change()
+        self.assertSceneEnter("test1", 2)
+        self.assertSceneLeave("test1", 1)
+        self.assertSceneEnter("test2", 1)
+        self.assertSceneLeave("test2", 1)
+        self.assertSceneCurrent("test1")
 
     def test_pop_and_change2(self):
         """
         Test pop and change scene where the scene stack is empty.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.start_scene("test1")
+
+        with self.assertRaises(IndexError):
+            self.sm.pop_and_change()
 
     def test_start_scene(self):
         """
         Test setting the start scene.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.assertEqual(self.sm.current_scene, None)
+
+        self.sm.start_scene("test1")
+        self.assertSceneCurrent("test1")
 
     def test_update(self):
         """
         Test calling update on the current scene.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.start_scene("test1")
 
-    def test_update(self):
+        self.assertSceneUpdate("test1", 0)
+        self.assertSceneUpdate("test2", 0)
+
+        context = GMContext()
+        self.sm.update(context)
+        self.assertSceneUpdate("test1", 1)
+        self.assertSceneUpdate("test2", 0)
+        self.sm.change_to_scene("test2")
+        self.sm.update(context)
+        self.assertSceneUpdate("test1", 1)
+        self.assertSceneUpdate("test2", 1)
+
+    def test_draw(self):
         """
         Test calling draw on the current scene.
         """
-        pass
+        self.sm.add_scene(TestScene("test1"))
+        self.sm.add_scene(TestScene("test2"))
+        self.sm.start_scene("test1")
 
+        self.assertSceneDraw("test1", 0)
+        self.assertSceneDraw("test2", 0)
+
+        context = GMContext()
+        self.sm.draw(context)
+        self.assertSceneDraw("test1", 1)
+        self.assertSceneDraw("test2", 0)
+        self.sm.change_to_scene("test2")
+        self.sm.draw(context)
+        self.assertSceneDraw("test1", 1)
+        self.assertSceneDraw("test2", 1)
 
 if __name__ == '__main__':
     unittest.main()
