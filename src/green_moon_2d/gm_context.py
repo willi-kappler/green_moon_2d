348e9b4f9c65f016c917fdf9898894fd65b0b202
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-02 21:01:39 +0200

    Fix style, fix missing class member.

--- src/green_moon_2d/gm_context.py
+++ src/green_moon_2d/gm_context.py
@@ -4,17 +4,22 @@
 # See: https://github.com/willi-kappler/green_moon_2d
 
 """
-This module defines the GMContext class which is passed to all the update() and draw() methods.
+This module defines the GMContext class which is passed to all the update()
+and draw() methods.
 """
+
+from typing import Any
+
 
 class GMContext:
     """
-    This class contains acess to the scene manager and the state of the game engine.
-    It also allows you to store data that is used across multiple scenes.
+    This class contains acess to the scene manager and the state of the
+    game engine. It also allows you to store data that is used across
+    multiple scenes.
     """
+
     def __init__(self):
-        game_property = {}
+        self.game_property = {}
 
-
-
-
+    def set_property(self, name: str, val: Any) -> None:
+        self.game_property[name] = val
