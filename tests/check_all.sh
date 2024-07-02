4abb64e3f0ac0a85bfe2ed93828962cf9409e300
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-02 14:49:29 +0200

    Add missing PYTHONPATH, add checks for tests.

--- tests/check_all.sh
+++ tests/check_all.sh
@@ -2,15 +2,20 @@
 
 reset
 
+export PYTHONPATH=$PYTHONPATH:"src/"
+
 echo "ruff:"
 ruff check src/green_moon_2d/
+ruff check tests/
 
 echo -e "\n\n-------------------------------------------\n\n"
 
 echo "mypy:"
 mypy src/green_moon_2d/
+mypy tests/
 
 echo -e "\n\n-------------------------------------------\n\n"
 
 echo "flake8:"
 flake8 src/green_moon_2d/
+flake8 tests/
