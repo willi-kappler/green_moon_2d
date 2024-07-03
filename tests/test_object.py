9b52ee4aa4ad47219a6414d08af292ad7406efb4
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-07-03 17:47:06 +0200

    Add more test cases.

--- tests/test_object.py
+++ tests/test_object.py
@@ -5,6 +5,8 @@
 
 import unittest
 
+from typing import Any
+
 from green_moon_2d.gm_object import GMObject, GMObjectManager
 from green_moon_2d.gm_context import GMContext
 
@@ -35,6 +37,9 @@ class TestObjectManager(unittest.TestCase):
 
     def assertObjectGroups(self, name: str, groups: list[str]) -> None:
         self.assertEqual(self.om.get(name).groups, set(groups))
+
+    def assertObjectProperty(self, name: str, property: str, val: Any):
+        self.assertEqual(self.om.get(name).properties[property], val)
 
     def test_add_object1(self):
         """
@@ -234,8 +239,7 @@ class TestObjectManager(unittest.TestCase):
 
     def test_remove_group1(self):
         """
-        Test removing a group from an object and removing an unknown group
-        from an object.
+        Test removing a group from an object.
         """
         self.om.add(TestObject("test1"))
         self.om.add(TestObject("test2"))
@@ -255,8 +259,10 @@ class TestObjectManager(unittest.TestCase):
         self.assertObjectGroups("test1", ["bar"])
         self.assertObjectGroups("test2", ["green", "blue"])
 
-        with self.assertRaises(KeyError):
-            self.om.remove_group("test2", "yellow")
+        self.om.remove_group("test2", "yellow")
+
+        self.assertObjectGroups("test1", ["bar"])
+        self.assertObjectGroups("test2", ["green", "blue"])
 
     def test_remove_group2(self):
         """
@@ -269,23 +275,80 @@ class TestObjectManager(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.om.remove_group("test3", "foo")
 
+    def test_remove_group_from_all(self):
+        """
+        Test removing a group from all objects
+        """
+
+        self.om.add(TestObject("test1"))
+        self.om.add(TestObject("test2"))
+        self.om.add(TestObject("test3"))
+
+        self.om.add_group("test1", ["foo", "bar", "top"])
+        self.om.add_group("test2", ["green", "blue", "yellow", "top"])
+        self.om.add_group("test3", ["poison"])
+
+        self.assertObjectGroups("test1", ["foo", "bar", "top"])
+        self.assertObjectGroups("test2", ["green", "blue", "yellow", "top"])
+        self.assertObjectGroups("test3", ["poison"])
+
+        self.om.remove_group_from_all("top")
+
+        self.assertObjectGroups("test1", ["foo", "bar"])
+        self.assertObjectGroups("test2", ["green", "blue", "yellow"])
+        self.assertObjectGroups("test3", ["poison"])
+
     def test_clear_groups1(self):
         """
         Test removing all groups from an object.
         """
-        pass
+        self.om.add(TestObject("test1"))
+        self.om.add(TestObject("test2"))
+
+        self.om.add_group("test1", ["foo", "bar", "top"])
+        self.om.add_group("test2", ["green", "blue", "yellow", "top"])
+
+        self.assertObjectGroups("test1", ["foo", "bar", "top"])
+        self.assertObjectGroups("test2", ["green", "blue", "yellow", "top"])
+
+        self.om.clear_groups("test1")
+
+        self.assertObjectGroups("test1", [])
+        self.assertObjectGroups("test2", ["green", "blue", "yellow", "top"])
 
     def test_clear_groups2(self):
         """
         Test removing all groups from an unknown object.
         """
-        pass
+        self.om.add(TestObject("test1"))
+        self.om.add(TestObject("test2"))
+
+        with self.assertRaises(KeyError):
+            self.om.clear_groups("test3")
 
     def test_iter_group1(self):
         """
         Test iterating over all objects that belong to a group.
         """
-        pass
+
+        self.om.add(TestObject("test1"))
+        self.om.add(TestObject("test2"))
+        self.om.add(TestObject("test3"))
+
+        self.om.add_group("test1", ["foo", "bar", "top"])
+        self.om.add_group("test2", ["green", "blue", "yellow"])
+        self.om.add_group("test3", ["bleeding", "top"])
+
+        self.om.get("test1").properties["Called"] = "No"
+        self.om.get("test2").properties["Called"] = "No"
+        self.om.get("test3").properties["Called"] = "No"
+
+        for ob in self.om.iter_group("top"):
+            ob.properties["Called"] = "Yes"
+
+        self.assertObjectProperty("test1", "Called", "Yes")
+        self.assertObjectProperty("test2", "Called", "No")
+        self.assertObjectProperty("test3", "Called", "Yes")
 
     def test_iter_group2(self):
         """
