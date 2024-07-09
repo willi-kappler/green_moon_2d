98373284a1608a7c13dfa063cf7a982332f5d736
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-09 22:41:25 +0200

    Add override decorator for methods.

--- src/green_moon_2d/gm_sprite.py
+++ src/green_moon_2d/gm_sprite.py
@@ -3,7 +3,7 @@
 #
 # See: https://github.com/willi-kappler/green_moon_2d
 
-from typing import Any
+from typing import Any, override
 
 from green_moon_2d.gm_context import GMContext
 from green_moon_2d.gm_object import GMObject
@@ -13,14 +13,15 @@ class GMSprite(GMObject):
     def __init__(self, name: str):
         super().__init__(name)
 
+    @override
     def draw(self, context: GMContext) -> None:
-        context.clear_screen()
         raise NotImplementedError
 
+    @override
     def update(self, context: GMContext) -> None:
-        context.clear_screen()
         raise NotImplementedError
 
+    @override
     def send_message(self, msg: Any) -> Any:
         if isinstance(msg, str):
             raise NotImplementedError
