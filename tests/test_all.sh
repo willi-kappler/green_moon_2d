f74f6c345ef0b4ee617b8ba40e2d211b15df4e40
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-09 22:45:25 +0200

    Add delimiter after each test.

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -5,6 +5,7 @@ reset
 export PYTHONPATH=$PYTHONPATH:"src/"
 
 for t in tests/*.py; do
-	echo $t
-	python3 $t
+    echo $t
+    python3 $t
+    echo -e "###########################################################\n"
 done
