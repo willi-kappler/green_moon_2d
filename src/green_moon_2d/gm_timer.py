508e821d74c10ae6ea09815b6dde9a7570cbf52f
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-07-11 17:54:16 +0200

    Add documentation.

--- src/green_moon_2d/gm_timer.py
+++ src/green_moon_2d/gm_timer.py
@@ -7,12 +7,22 @@ import time
 
 
 class GMTimer:
+    """
+    This class defines a timer that is set with a duration in ms.
+    """
+
     def __init__(self, duration: int):
         self.duration: int = duration
         self.start_time: int = int(time.time() * 1000.0)
         self.active = True
 
     def finished(self) -> bool:
+        """
+        When the timer is active, checks if the timer is finished.
+
+        :return: True if finished otherwise False. Also False if the timer is inactive.
+        :rtype: bool
+        """
         if self.active:
             current_time = int(time.time() * 1000.0)
             return (current_time - self.start_time) > self.duration
@@ -20,13 +30,26 @@ class GMTimer:
             return False
 
     def set_duration(self, duration: int) -> None:
+        """
+        Sets the new duration for this timer. Note that it is not restarted automatically.
+
+        :param int duration: The new duration in ms.
+        """
         self.duration = duration
 
     def set_duration_restart(self, duration: int) -> None:
+        """
+        Sets the new duration and restarts the timer.
+
+        :param int duration: The new duration in ms.
+        """
         self.duration = duration
         self.restart()
 
     def restart(self) -> None:
+        """
+        Restarts the timer and set it to active.
+        """
         self.start_time = int(time.time() * 1000.0)
         self.active = True
 
