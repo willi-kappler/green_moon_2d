a9837b9099ffbb6e22745c99492e7592c908122c
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-07-11 17:55:20 +0200

    Add first test for GMVec2D

--- tests/test_math.py
+++ tests/test_math.py
@@ -5,5 +5,30 @@
 
 import unittest
 
+import green_moon_2d.gm_math as gmath
+
+
+class TestVec2D(unittest.TestCase):
+    def test_add(self):
+        v1 = gmath.GMVec2D(1.0, 4.0)
+        v2 = gmath.GMVec2D(2.5, -3.5)
+        v3 = v1 + v2
+
+        self.assertEqual(v3, gmath.GMVec2D(3.5, 0.5))
+
+        v1 = gmath.GMVec2D(2.7, -5.2)
+        v2 = gmath.GMVec2D(0.0, 0.0)
+        v3 = v1 + v2
+
+        self.assertEqual(v3, gmath.GMVec2D(2.7, -5.2))
+
+        v1 = gmath.GMVec2D(0.0, 0.0)
+        v2 = gmath.GMVec2D(9.9, 7.1)
+        v3 = v1 + v2
+
+        self.assertEqual(v3, gmath.GMVec2D(9.9, 7.1))
+
+
 if __name__ == "__main__":
     unittest.main()
+
