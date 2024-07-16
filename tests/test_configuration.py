8e95a4c42b8eeda818c16839cdc7c57296dc9360
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-16 21:05:53 +0200

    Add stub for configuration tests.

---
+++ tests/test_configuration.py
@@ -0,0 +1,19 @@
+# This file is part of GreenMoon2D, a 2D game engine for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/green_moon_2d
+
+import unittest
+
+from green_moon_2d.gm_configuration import GMConfiguration
+
+
+class TestConfiguration(unittest.TestCase):
+    def test_load_configuration1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
