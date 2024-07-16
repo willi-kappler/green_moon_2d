6c75ab4bd75acfb33f4d47c0fb3c17dc4f4a7e43
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-16 15:46:45 +0200

    Fix circular import error, add scene manager methods / messages.

--- src/green_moon_2d/gm_context.py
+++ src/green_moon_2d/gm_context.py
@@ -4,17 +4,17 @@
 # See: https://github.com/willi-kappler/green_moon_2d
 
 """
-This module defines the GMContext class which is passed to all the update()
-and draw() methods.
+This module defines the GMContext class which contain all game engine relevant elements.
 """
 
 from typing import Any
 
-import sdl2
-import sdl2.ext
+import sdl2  # type: ignore
+import sdl2.ext  # type: ignore
 
 from green_moon_2d.gm_configuration import GMConfiguration
-from green_moon_2d.gm_scene import GMSceneManager
+from green_moon_2d.gm_interfaces import GMSceneInterface
+from green_moon_2d.gm_messages import GMSceneMessage
 
 
 class GMContext:
@@ -27,9 +27,9 @@ class GMContext:
     def __init__(self):
         self.game_property: dict[str, Any] = {}
         self.config = GMConfiguration()
-        self.scene_manager: GMSceneManager = GMSceneManager()
         self.quit_game: bool = False
         self.renderer: Any = None
+        self.scene_messages: list[GMSceneMessage] = []
 
     def set_property(self, name: str, val: Any) -> None:
         """
@@ -38,6 +38,7 @@ class GMContext:
         :param name: The name of the property.
         :param val: The value of the property.
         """
+
         self.game_property[name] = val
 
     def get_property(self, name: str) -> Any:
@@ -49,6 +50,7 @@ class GMContext:
         :rtype: Any
         :raises NameError: if the property with the given name was not found.
         """
+
         return self.game_property[name]
 
     def has_property(self, name: str) -> bool:
@@ -60,6 +62,7 @@ class GMContext:
         :return: If the property with the given name is available return True otherwise False.
         :rtype: bool
         """
+
         return name in self.game_property
 
     def load_config(self, file_name: str):
@@ -68,6 +71,7 @@ class GMContext:
 
         :param file_name: The name of the configuration file.
         """
+
         self.config.load_config(file_name)
 
     def load_resources(self):
@@ -78,7 +82,7 @@ class GMContext:
         raise NotImplementedError
         # TODO: Load resources from JSON file.
 
-    def set_fullscreen(self):
+    def set_screen_mode(self):
         """
         Sets the window to full screen or windowed mode, depenting on the setting in the config option.
         """
@@ -94,21 +98,65 @@ class GMContext:
         """
 
         self.config.fullscreen = not self.config.fullscreen
-        self.set_fullscreen()
+        self.set_screen_mode()
 
-    def update(self):
+    def clear(self):
         """
-        Updates the current scene in the scene manager.
-        """
-
-        self.scene_manager.update(self)
-
-    def draw(self):
-        """
-        Draws the current scene in the scene manager.
+        Clears the screen with black color.
         """
 
-        self.scene_manager.draw(self)
+        self.renderer.clear()
+
+    def present(self):
+        """
+        Updates the screen with all changes (batched mode).
+        """
+
+        self.renderer.present()
+
+    def add_scene(self, scene: GMSceneInterface):
+        """
+        Creates a message to add a new scene to the scene manager.
+
+        :param scene: The new scene to be added.
+        """
+
+        self.scene_messages.append(GMSceneMessage("add", scene=scene))
+
+    def delete_scene(self, name: str):
+        """
+        Create a message to elete the scene with the given name.
+
+        :param name: The name of the scene to be deleted.
+        """
+
+        self.scene_messages.append(GMSceneMessage("delete", scene_name=name))
+
+    def change_scene(self, name: str):
+        """
+        Changes the current scene to the named scene.
+
+        :param name: The name of the scene to change to.
+        """
+
+        self.scene_messages.append(GMSceneMessage("change", scene_name=name))
+
+    def push_and_change_scene(self, name: str):
+        """
+        Creates a message to push the current scene onto the stack and change to the given scene.
+
+        :param name: The name of the scene to be changed to.
+        """
+
+        self.scene_messages.append(GMSceneMessage("push", scene_name=name))
+
+    def pop_scene(self):
+        """
+        Creates a message to pop the scene from the stack and switch to it.
+        """
+
+        self.scene_messages.append(GMSceneMessage("pop"))
+
 
 
 
