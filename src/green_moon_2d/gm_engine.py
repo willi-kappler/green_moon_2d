dd61d0a243639a3716df057006950e108fd68167
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-16 16:14:20 +0200

    Add more functions to the engine, add sdl2 init function calls.

--- src/green_moon_2d/gm_engine.py
+++ src/green_moon_2d/gm_engine.py
@@ -3,17 +3,18 @@
 #
 # See: https://github.com/willi-kappler/green_moon_2d
 
-import sdl2
-import sdl2.ext
+import sdl2  # type: ignore
+import sdl2.ext  # type: ignore
 
 from green_moon_2d.gm_configuration import GMConfiguration
 from green_moon_2d.gm_context import GMContext
-from green_moon_2d.gm_scene import GMScene
+from green_moon_2d.gm_scene import GMScene, GMSceneManager
 
 
 class GMEngine:
     def __init__(self, config_file: str = ""):
         self.context: GMContext = GMContext()
+        self.scene_manager = GMSceneManager()
 
         if len(config_file) > 0:
             self.context.load_config(config_file)
@@ -40,7 +41,7 @@ class GMEngine:
 
         :param scene: The new scene to be added.
         """
-        self.context.scene_manager.add_scene(scene)
+        self.scene_manager.add_scene(scene)
 
     def run(self):
         """
@@ -49,23 +50,23 @@ class GMEngine:
 
         config = self.context.config
         ctx = self.context
+        smgr = self.scene_manager
 
         sdl2.ext.init(video=True, audio=True, timer=True, controller=True)
         window = sdl2.ext.Window(config.window_title, (config.screen_width, config.screen_height))
 
         render_flags = (sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC)
         ctx.renderer = sdl2.ext.Renderer(window, flags=render_flags)
-
-        ctx.set_fullscreen()
+        ctx.set_screen_mode()
 
 
         window.show()
 
         while not ctx.quit_game:
-            ctx.renderer.clear()
-            ctx.update()
-            ctx.draw()
-            ctx.renderer.present()
+            smgr.update(ctx)
+            ctx.clear()
+            smgr.draw(ctx)
+            ctx.present()
 
         window.close()
         sdl2.ext.quit()
