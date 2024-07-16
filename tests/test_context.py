022f0ad0ceb3891a6c59d763a93c63bb177b444a
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-16 21:06:03 +0200

    Add stub for context tests.

---
+++ tests/test_context.py
@@ -0,0 +1,19 @@
+# This file is part of GreenMoon2D, a 2D game engine for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/green_moon_2d
+
+import unittest
+
+from green_moon_2d.gm_context import GMContext
+
+
+class TestContext(unittest.TestCase):
+    def test_load_context1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
