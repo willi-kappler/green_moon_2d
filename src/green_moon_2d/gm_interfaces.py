8d07fa5c4162acf566f017c448843049432f6491
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-16 16:14:43 +0200

    Add interface module as work around for cyclic import / class error.

---
+++ src/green_moon_2d/gm_interfaces.py
@@ -0,0 +1,14 @@
+# This file is part of GreenMoon2D, a 2D game engine for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/green_moon_2d
+
+"""
+This module defines various interfaces as a work around to the cyclic imports / classes error.
+"""
+
+
+class GMSceneInterface:
+    pass
+
+
