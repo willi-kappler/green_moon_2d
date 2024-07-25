3229b95de1793ca6f46e87647f82f155ce07bfd2
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-25 23:53:08 +0200

    Add initial code for GMFont.

--- src/green_moon_2d/gm_font.py
+++ src/green_moon_2d/gm_font.py
@@ -3,3 +3,16 @@
 #
 # See: https://github.com/willi-kappler/green_moon_2d
 
+from green_moon_2d.gm_context import GMContext
+from green_moon_2d.gm_texture import GMTexture
+
+
+class GMFont:
+    def __init__(self, texture: GMTexture, mapping: dict[str, int]):
+        self.texture = texture
+        self.mapping = mapping
+
+    def draw(self, x: float, y: float, c: str, context: GMContext):
+        i = self.mapping[c]
+        self.texture.draw(x, y, i, context)
+
