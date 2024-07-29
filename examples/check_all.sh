43062360bbce23eb3276fdff88dea0f9d6f7e1a9
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-29 14:15:45 +0200

    Add example text2.

--- examples/check_all.sh
+++ examples/check_all.sh
@@ -4,7 +4,7 @@ reset
 
 export PYTHONPATH=$PYTHONPATH:"../src/"
 
-FOLDERS=("text1a" "text1b")
+FOLDERS=("text1a" "text1b" "text2")
 
 for f in ${FOLDERS[@]}; do
     echo "---------- New folder: $f"
