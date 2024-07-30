1c019403ba9111b81edad924dcf9e872d7f77905
Willi Kappler <willi.kappler.gm@gmail.com>
2024-07-30 23:11:18 +0200

    Add first code for sprite.

--- src/green_moon_2d/gm_sprite.py
+++ src/green_moon_2d/gm_sprite.py
@@ -6,23 +6,38 @@
 from typing import Any, override
 
 from green_moon_2d.gm_object import GMObject
+from green_moon_2d.gm_animation import GMAnimation
+from green_moon_2d.gm_texture import GMTexture
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class GMSprite(GMObject):
-    def __init__(self, name: str):
+    def __init__(self, name: str, animation: GMAnimation, texture: GMTexture):
         """
         :param name: The name of the sprite. It must be unique.
+        :param animation: The animation for this sprite.
+        :param texture: The texture / sprite sheet for this sprite.
         """
 
         super().__init__(name)
 
+        self.animation: GMAnimation = animation
+        self.texture: GMTexture = texture
+        self.angle: float = 0.0
+        self.scale: float = 1.0
+        self.flip_x: bool = False
+        self.flip_y: bool = False
+
     @override
     def draw(self) -> None:
-        raise NotImplementedError
+        index = self.animation.get_frame_index()
+        self.texture.draw_p_opt(self.pos, index, self.angle, self.scale, self.flip_x, self.flip_y)
 
     @override
     def update(self) -> None:
-        raise NotImplementedError
+        self.animation.update()
 
     @override
     def send_message(self, msg: Any) -> Any:
