fe539b9ab3d4d0b4573c202509392bcfa1198840
Willi Kappler <willi.kappler.gm@gmail.com>
2025-03-10 23:45:18 +0100

    Add message util classes.

---
+++ src/green_moon_2d/gm_msg_util.py
@@ -0,0 +1,73 @@
+# This file is part of GreenMoon2D, a 2D game engine for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/green_moon_2d
+
+from typing import Any, override
+
+import green_moon_2d.gm_math as gmm
+from green_moon_2d.gm_message import GMMessage
+from green_moon_2d.gm_object import GMObject, GMObjectManager
+from green_moon_2d.gm_interpolation import GMInterpolate
+
+import logging
+logger = logging.getLogger(__name__)
+
+class GMMUIgnore(GMObject):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    @override
+    def send_message(self, msg: GMMessage) -> Any:
+        # Just ignore all messages
+        pass
+
+
+class GMMUMap(GMObject):
+    def __init__(self, name: str, func, om: GMObjectManager):
+        super().__init__(name)
+        self.func = func
+        self.om: GMObjectManager = om
+        self.user_message: GMMessage = GMMessage.empty()
+
+    @override
+    def send_message(self, msg: GMMessage) -> Any:
+
+        child: str = msg.next_child()
+
+        match child:
+            case "base":
+                return super().send_message(msg)
+            case "msg":
+                return self.user_message.send_message(msg)
+            case "map_self":
+                if msg.command == "set_func":
+                    self.func = msg.value
+                else:
+                    raise ValueError(f"Unknown message: {msg}, {child=}")
+            case _:
+                self.message_received = True
+                # Apply function to message value:
+                self.user_message.value = self.func(msg.value)
+                # Copy rest of message:
+                self.user_message.command = msg.command
+                # Restore child hirarchy:
+                msg.insert_child(child)
+                self.user_message.children = msg.children
+                return self.om.send_message(self.user_message)
+
+class GMMUTimer(GMObject):
+    def __init__(self, name: str, delay: float):
+        super().__init__(name)
+        self.timer = GMMUTimer("mu_timer", delay)
+
+    @override
+    def update(self, dt: float, om) -> None:
+        pass
+
+    @override
+    def send_message(self, msg: GMMessage) -> Any:
+        pass
+
+
+
