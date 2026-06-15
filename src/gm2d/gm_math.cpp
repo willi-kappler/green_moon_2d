544b2833414763dae6a023ffc1c9f4b82d3dec02
Willi Kappler <willi.kappler.gm@gmail.com>
2026-06-15 22:14:43 +0200

    Add enums for alignment and repetition. Add constructor for pair and array. Add circle and rectangle class.

--- src/gm2d/gm_math.cpp
+++ src/gm2d/gm_math.cpp
@@ -15,6 +15,8 @@
 #include "gm_math.hpp"
 
 namespace gm2d {
+
+// GMVec2D:
 GMVec2D::GMVec2D():
     x(0.0f32),
     y(0.0f32)
@@ -35,19 +37,25 @@ GMVec2D::GMVec2D(const GMVec2D &other):
     y(other.y)
 {}
 
-void GMVec2D::add(std::float32_t a) {
-    x += a;
-    y += a;
-}
+GMVec2D::GMVec2D(const std::pair<std::float32_t, std::float32_t> &p):
+    x(p.first),
+    y(p.second)
+{}
 
-void GMVec2D::add(std::float32_t a, std::float32_t b) {
-    x += a;
-    y += b;
-}
+GMVec2D::GMVec2D(const std::array<std::float32_t, 2> &a):
+    x(a[0]),
+    y(a[1])
+{}
 
-void GMVec2D::add(const GMVec2D &other) {
+
+void GMVec2D::add(const GMVec2D other) {
     x += other.x;
     y += other.y;
+}
+
+void GMVec2D::add(const std::float32_t xx, const std::float32_t yy) {
+    x += xx;
+    y += yy;
 }
 
 void GMVec2D::mul(std::float32_t a) {
@@ -81,6 +89,10 @@ void GMVec2D::norm2() {
     y = y / l;
 }
 
+[[nodiscard]] std::float32_t GMVec2D::dist(const GMVec2D &other) {
+    return static_cast<std::float32_t>(hypot(x - other.x, y - other.y));
+}
+
 [[nodiscard]] GMVec2D GMVec2D::rotate1(std::float32_t a) {
     /*
     std::float32_t rad = std::numbers::pi * a / 180.0;
@@ -109,53 +121,19 @@ void GMVec2D::rotate2(std::float32_t a) {
     y = y2;
 }
 
-void GMVec2D::operator=(const GMVec2D &other) {
+void GMVec2D::operator=(const GMVec2D other) {
     x = other.x;
     y = other.y;
 }
 
-[[nodiscard]] bool GMVec2D::operator==(const GMVec2D &other) {
-    return (x == other.x) && (y == other.y);
-}
-
-[[nodiscard]] bool GMVec2D::operator!=(const GMVec2D &other) {
-    return (x != other.x) || (y != other.y);
-}
-
-[[nodiscard]] GMVec2D GMVec2D::operator+(const GMVec2D &other) {
-    return GMVec2D(x + other.x, y + other.y);
-}
-
-[[nodiscard]] GMVec2D GMVec2D::operator+(const std::float32_t a) {
-    return GMVec2D(x + a, y + a);
-}
-
-void GMVec2D::operator+=(const GMVec2D &other) {
+void GMVec2D::operator+=(const GMVec2D other) {
     x += other.x;
     y += other.y;
 }
 
-void GMVec2D::operator+=(const std::float32_t a) {
-    x += a;
-    y += a;
-}
-
-[[nodiscard]] GMVec2D GMVec2D::operator-(const GMVec2D &other) {
-    return GMVec2D(x - other.x, y - other.y);
-}
-
-[[nodiscard]] GMVec2D GMVec2D::operator-(const std::float32_t a) {
-    return GMVec2D(x - a, y - a);
-}
-
-void GMVec2D::operator-=(const GMVec2D &other) {
+void GMVec2D::operator-=(const GMVec2D other) {
     x -= other.x;
     y -= other.y;
-}
-
-void GMVec2D::operator-=(const std::float32_t a) {
-    x -= a;
-    y -= a;
 }
 
 [[nodiscard]] GMVec2D GMVec2D::operator*(const std::float32_t a) {
@@ -167,8 +145,82 @@ void GMVec2D::operator*=(const std::float32_t a) {
     y *= a;
 }
 
+[[nodiscard]] bool operator==(const GMVec2D v1, const GMVec2D v2) {
+    return (v1.x == v2.x) && (v1.y == v2.y);
+}
+
+[[nodiscard]] bool operator!=(const GMVec2D v1, const GMVec2D v2) {
+    return (v1.x != v2.x) || (v1.y != v2.y);
+}
+
+[[nodiscard]] GMVec2D operator+(const GMVec2D v1, const GMVec2D v2) {
+    return GMVec2D(v1.x + v2.x, v1.y + v2.y);
+}
+
+[[nodiscard]] GMVec2D operator-(const GMVec2D v1, const GMVec2D v2) {
+    return GMVec2D(v1.x - v2.x, v1.y - v2.y);
+}
+
+// GMCircle:
+GMCircle::GMCircle():
+    cx(0.0f32),
+    cy(0.0f32),
+    r(0.0f32)
+{}
+
+GMCircle::GMCircle(const std::float32_t x, const std::float32_t y, const std::float32_t radius):
+    cx(x),
+    cy(y),
+    r(radius)
+{}
+
+GMCircle::GMCircle(const GMVec2D &v, const std::float32_t radius):
+    cx(v.x),
+    cy(v.y),
+    r(radius)
+{}
+
+GMCircle::GMCircle(const std::pair<std::float32_t, std::float32_t> &p, const std::float32_t radius):
+    cx(p.first),
+    cy(p.second),
+    r(radius)
+{}
+
+GMCircle::GMCircle(const std::array<std::float32_t, 2> &a, const std::float32_t radius):
+    cx(a[0]),
+    cy(a[1]),
+    r(radius)
+{}
+
+// GMRectangle:
+GMRectangle::GMRectangle():
+    x1(0.0f32),
+    y1(0.0f32),
+    x2(0.0f32),
+    y2(0.0f32)
+{}
+GMRectangle::GMRectangle(const std::float32_t xx1, const std::float32_t yy1, const std::float32_t xx2, const std::float32_t yy2):
+    x1(xx1),
+    y1(yy1),
+    x2(xx2),
+    y2(yy2)
+{}
+GMRectangle::GMRectangle(const GMVec2D &v1, const GMVec2D &v2):
+    x1(v1.x),
+    y1(v1.y),
+    x2(v2.x),
+    y2(v2.y)
+{}
+GMRectangle::GMRectangle(const std::pair<std::float32_t, std::float32_t> &p1, const std::pair<std::float32_t, std::float32_t> &p2):
+    x1(p1.first),
+    y1(p1.second),
+    x2(p2.first),
+    y2(p2.second)
+{}
+
+
+
 [[nodiscard]] bool approx(std::float32_t a, std::float32_t b) {
     return std::abs(a - b) <= 0.00001f32;
 }
-
 }
--- src/gm2d/gm_math.hpp
+++ src/gm2d/gm_math.hpp
@@ -11,78 +11,116 @@
 #define FILE_GM_MATH_HPP_INCLUDED
 
 // STD includes:
+#include <cstdint>
 #include <stdfloat>
+#include <utility>
+#include <array>
 
 namespace gm2d {
+enum struct GMAlignment: uint8_t {
+    TOP_LEFT = 0,
+    TOP_CENTER,
+    TOP_RIGHT,
+
+    MID_LEFT,
+    MID_CENTER,
+    MID_RIGHT,
+
+    BTM_LEFT,
+    BTM_CENTER,
+    BTM_RIGHT
+};
+
+enum struct GMRepetition: uint8_t {
+    FIXED = 0,
+    FORWARD,
+    BACKWARD,
+    FORWARD_LOOP,
+    BACKWARD_LOOP,
+    PINGPONG_F,
+    PINGPONG_B
+};
+
 class GMVec2D {
     public:
         // Constructor:
         GMVec2D();
-        GMVec2D(std::float32_t);
-        GMVec2D(std::float32_t, std::float32_t);
+        GMVec2D(const std::float32_t);
+        GMVec2D(const std::float32_t, const std::float32_t);
         GMVec2D(const GMVec2D &);
+        GMVec2D(const std::pair<std::float32_t, std::float32_t> &);
+        GMVec2D(const std::array<std::float32_t, 2> &);
 
         // Methods:
-        void add(std::float32_t);
-        void add(std::float32_t, std::float32_t);
-        void add(const GMVec2D &);
-
-        void mul(std::float32_t);
+        void add(const GMVec2D);
+        void add(const std::float32_t, const std::float32_t);
+        void mul(const std::float32_t);
 
         [[nodiscard]] std::float32_t len1();
         [[nodiscard]] std::float32_t len2();
 
         [[nodiscard]] GMVec2D norm1();
         void norm2();
+        [[nodiscard]] std::float32_t dist(const GMVec2D &);
 
-        [[nodiscard]] GMVec2D rotate1(std::float32_t);
+        [[nodiscard]] GMVec2D rotate1(const std::float32_t);
         void rotate2(std::float32_t);
 
         // Operators:
-        void operator=(const GMVec2D &);
-
-        [[nodiscard]] bool operator==(const GMVec2D &);
-        [[nodiscard]] bool operator!=(const GMVec2D &);
-
-        [[nodiscard]] GMVec2D operator+(const GMVec2D &);
-        [[nodiscard]] GMVec2D operator+(const std::float32_t);
-        void operator+=(const GMVec2D &);
-        void operator+=(const std::float32_t);
-
-        [[nodiscard]] GMVec2D operator-(const GMVec2D &);
-        [[nodiscard]] GMVec2D operator-(const std::float32_t);
-        void operator-=(const GMVec2D &);
-        void operator-=(const std::float32_t);
+        void operator=(const GMVec2D);
+        void operator+=(const GMVec2D);
+        void operator-=(const GMVec2D);
 
         [[nodiscard]] GMVec2D operator*(const std::float32_t);
         void operator*=(const std::float32_t);
+
+        friend bool operator==(const GMVec2D, const GMVec2D);
+        friend bool operator!=(const GMVec2D, const GMVec2D);
+        friend GMVec2D operator+(const GMVec2D, const GMVec2D);
+        friend GMVec2D operator-(const GMVec2D, const GMVec2D);
 
         // Members:
         std::float32_t x;
         std::float32_t y;
 };
 
+class GMCircle {
+        // Constructor:
+        GMCircle();
+        GMCircle(const std::float32_t, const std::float32_t, const std::float32_t);
+        GMCircle(const GMVec2D &, const std::float32_t);
+        GMCircle(const std::pair<std::float32_t, std::float32_t> &, const std::float32_t);
+        GMCircle(const std::array<std::float32_t, 2> &, const std::float32_t);
+
+        // Methods:
+
+        // Operators:
+
+        // Members:
+        std::float32_t cx;
+        std::float32_t cy;
+        std::float32_t r;
+};
+
+class GMRectangle {
+        // Constructor:
+        GMRectangle();
+        GMRectangle(const std::float32_t, const std::float32_t, const std::float32_t, const std::float32_t);
+        GMRectangle(const GMVec2D &, const GMVec2D &);
+        GMRectangle(const std::pair<std::float32_t, std::float32_t> &, const std::pair<std::float32_t, std::float32_t> &);
+
+        // Methods:
+
+        // Operators:
+
+        // Members:
+        std::float32_t x1;
+        std::float32_t y1;
+        std::float32_t x2;
+        std::float32_t y2;
+};
+
 [[nodiscard]] bool approx(std::float32_t, std::float32_t);
 
 }
-
-/*
-// Operators:
-bool operator==(const gm2d::GMVec2D &, const gm2d::GMVec2D &);
-bool operator!=(const gm2d::GMVec2D &, const gm2d::GMVec2D &);
-
-gm2d::GMVec2D operator+(const gm2d::GMVec2D &, const gm2d::GMVec2D& );
-gm2d::GMVec2D operator+(const gm2d::GMVec2D &, const std::float32_t);
-void operator+=(gm2d::GMVec2D &, const gm2d::GMVec2D &);
-void operator+=(gm2d::GMVec2D &, const std::float32_t);
-
-gm2d::GMVec2D operator-(const gm2d::GMVec2D &, const gm2d::GMVec2D &);
-gm2d::GMVec2D operator-(const gm2d::GMVec2D &, const std::float32_t);
-void operator-=(gm2d::GMVec2D &, const gm2d::GMVec2D &);
-void operator-=(gm2d::GMVec2D &, const std::float32_t);
-
-gm2d::GMVec2D operator*(const gm2d::GMVec2D &, const std::float32_t);
-void operator*=(gm2d::GMVec2D &, const std::float32_t);
-*/
-
 #endif // FILE_GM_MATH_HPP_INCLUDED
