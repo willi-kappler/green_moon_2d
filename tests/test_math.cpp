9f42dbb04e0e7f7dc4ac9a32850ea116af0c277e
Willi Kappler <willi.kappler.gm@gmail.com>
2026-06-22 22:49:32 +0200

    Add test cases for line, circle and rectangle and some helper functions.

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -62,11 +62,17 @@ TEST_CASE("Create and use GMVec2D", "[math]" ) {
     v2 = {80.0f32, 60.0f32};
     REQUIRE(gm_approx(v1.gm_dist1(v2), 50.0f32));
     REQUIRE(gm_approx(v1.gm_dist2(v2), 2500.0f32));
-
-    REQUIRE(gm_approx(v1.gm_angle(), 68.19859f32));
-    REQUIRE(gm_approx(v2.gm_angle(), 53.13010f32));
     REQUIRE(gm_approx(v1.gm_cross(v2), 1400.0f32));
     REQUIRE(gm_approx(v1.gm_cross(v1), 0.0f32));
+
+    v1 = {10.0f32, 0.0f32};
+    REQUIRE(gm_approx(v1.gm_angle(), 0.0f32));
+    v1 = {10.0f32, 10.0f32};
+    REQUIRE(gm_approx(v1.gm_angle(), 45.0f32));
+    v1 = {0.0f32, 10.0f32};
+    REQUIRE(gm_approx(v1.gm_angle(), 90.0f32));
+    v1 = {-10.0f32, 10.0f32};
+    REQUIRE(gm_approx(v1.gm_angle(), 135.0f32));
 
     v1 = {5.0f32, 0.0f32};
     v2 = v1.gm_rotate1(90.0f32);
@@ -129,28 +135,80 @@ TEST_CASE("Create and use GMVec2D", "[math]" ) {
 }
 
 TEST_CASE("Create and use GMLine", "[math]" ) {
-    GMLine l1{};
-    GMLine l2{};
-    GMVec2D v1{};
-    // TODO: implement test cases.
+    GMLine l1{10.0f32, 5.0f32, 40.0f32, 45.0f32};
+    GMLine l2{10.0f32, 10.0f32, 60.0f32, 60.0f32};
+
+    REQUIRE(l1.gm_len1() == 50.0f32);
+    REQUIRE(l1.gm_len2() == 2500.0f32);
+
+    REQUIRE(l2.gm_len1() == 70.71068f32);
+
+    l2.gm_scale(0.1414213562373095f32);
+    REQUIRE(l2.gm_len1() == 10.0f32);
+    REQUIRE(l2.gm_angle() == 45.00f32);
+
+    l2.gm_rotate(45.0f32);
+
+    REQUIRE((l2.v1.x == 10.0f32) && (l2.v1.y == 10.0f32));
+    REQUIRE((l2.v2.x == 10.0f32) && (l2.v2.y == 20.0f32));
+    REQUIRE(l2.gm_angle() == 90.00f32);
+
+    GMLine l3{10.0f32, 10.0f32, 10.0f32, 20.0f32};
+    REQUIRE(l1 != l2);
+    REQUIRE(l2 == l3);
 }
 
 TEST_CASE("Create and use GMCircle", "[math]" ) {
-    GMCircle c1{};
-    GMCircle c2{};
-    GMVec2D v1{};
-    // TODO: implement test cases.
+    GMCircle c1{10.0f32, 10.0f32, 5.0f32};
+    GMCircle c2{10.0f32, 10.0f32, 15.0f32};
+
+    REQUIRE(c1 != c2);
+    c1.gm_scale(3.0f32);
+    REQUIRE(c1 == c2);
 }
 
 TEST_CASE("Create and use GMRectangle", "[math]" ) {
-    GMRectangle r1{};
-    GMRectangle r2{};
-    GMVec2D v1{};
-    // TODO: implement test cases.
+    GMRectangle r1{10.0f32, 10.0f32, 50.0f32, 40.0f32};
+    GMRectangle r2{20.0f32, 30.0f32, 60.0f32, 150.0f32};
+
+    REQUIRE(r1.gm_width() == 40.0f32);
+    REQUIRE(r1.gm_height() == 30.0f32);
+    REQUIRE(r1.gm_diagonal1() == 50.0f32);
+    REQUIRE(r1.gm_diagonal2() == 2500.0f32);
+    REQUIRE(r1.gm_min_point() == GMVec2D(10.0f32, 10.0f32));
+    REQUIRE(r1.gm_max_point() == GMVec2D(50.0f32, 40.0f32));
+    r1.gm_scale(2.0f32);
+    REQUIRE(r1.gm_width() == 80.0f32);
+    REQUIRE(r1.gm_height() == 60.0f32);
+    REQUIRE(r1.gm_diagonal1() == 100.0f32);
+    REQUIRE((r1.v1.x == 10.0f32) && (r1.v1.y == 10.0f32));
+    REQUIRE((r1.v2.x == 90.0f32) && (r1.v2.y == 70.0f32));
+
+    GMRectangle r3{10.0f32, 10.0f32, 90.0f32, 70.0f32};
+    REQUIRE(r1 != r2);
+    REQUIRE(r1 == r3);
 }
 
 TEST_CASE("Test helper functions", "[math]" ) {
-    // TODO: implement test cases.
+    REQUIRE(gm_approx(2.5f32, 2.5f32));
+    REQUIRE(gm_approx(2.5f32, 2.500000001f32));
+    REQUIRE(!gm_approx(2.5f32, 2.51f32));
+
+    REQUIRE(gm_approx(GMVec2D(1.5f32, 3.2f32), GMVec2D(1.5f32, 3.2f32)));
+    REQUIRE(gm_approx(GMVec2D(1.5f32, 3.2f32), GMVec2D(1.5f32, 3.20000001f32)));
+    REQUIRE(!gm_approx(GMVec2D(1.5f32, 3.2f32), GMVec2D(1.5f32, 3.21f32)));
+
+    REQUIRE(gm_is_on_segment(GMVec2D(2.0f32, 4.0f32), GMVec2D(1.0f32, 4.0f32), GMVec2D(7.0f32, 4.0f32)));
+    REQUIRE(!gm_is_on_segment(GMVec2D(0.5f32, 4.0f32), GMVec2D(1.0f32, 4.0f32), GMVec2D(7.0f32, 4.0f32)));
+    REQUIRE(!gm_is_on_segment(GMVec2D(8.0f32, 4.0f32), GMVec2D(1.0f32, 4.0f32), GMVec2D(7.0f32, 4.0f32)));
+    REQUIRE(!gm_is_on_segment(GMVec2D(2.0f32, 3.0f32), GMVec2D(1.0f32, 4.0f32), GMVec2D(7.0f32, 4.0f32)));
+    REQUIRE(gm_is_on_segment(GMVec2D(2.0f32, 2.0f32), GMVec2D(1.0f32, 1.0f32), GMVec2D(7.0f32, 7.0f32)));
+
+    // gm_orientation
+
+    // gm_between
+
+    // gm_closest_point_on_segment
 }
 
 TEST_CASE("Test intersect functions", "[math]" ) {
