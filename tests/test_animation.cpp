7eff5f4e457c30d9234eb0b7e3118bbe5e69c447
Willi Kappler <willi.kappler@uni-tuebingen.de>
2026-07-02 16:37:49 +0200

    Add test case stub for gm_animation.

---
+++ tests/test_animation.cpp
@@ -0,0 +1,26 @@
+/*
+    GreenMoon2D
+    SPDX-License-Identifier: MIT
+    Written by Willi Kappler, MIT License
+    https://github.com/willi-kappler/green_moon_2d
+
+    This file contains the tests for the animation class.
+
+    Run only configuration tests:
+    xmake run -w ./ gm2d_test [animation]
+*/
+
+// STD includes:
+#include <thread>
+
+// External includes:
+#include <snitch/snitch.hpp>
+
+// Local includes:
+#include "gm2d/gm_animation.hpp"
+
+using namespace gm2d;
+
+TEST_CASE("Create Animation", "[animation]" ) {
+}
+
