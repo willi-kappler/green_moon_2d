53a48abc7964b10209562cb45240eed4538ff4b1
Willi Kappler <willi.kappler@uni-tuebingen.de>
2026-07-02 17:11:25 +0200

    Add some test cases for animation.

--- tests/test_animation.cpp
+++ tests/test_animation.cpp
@@ -21,6 +21,44 @@
 
 using namespace gm2d;
 
-TEST_CASE("Create Animation", "[animation]" ) {
+TEST_CASE("Create animation", "[animation]" ) {
+    GMAnimation a1{{5, 100}, {10, 100}, {8, 300}};
+    REQUIRE(a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 5);
+
+    std::vector<std::pair<uint16_t, uint16_t>> frames{{5, 100}, {10, 100}, {8, 300}};
+    GMAnimation a2(frames);
+    REQUIRE(a2.gm_finished());
+    REQUIRE(a2.gm_get_frame_index() == 5);
 }
 
+TEST_CASE("Change animation repetition", "[animation]" ) {
+    GMAnimation a1{{5, 100}, {10, 100}, {8, 300}};
+    a1.gm_change_repetition(GMRepetition::FIXED);
+    REQUIRE(a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 5);
+
+    a1.gm_change_repetition(GMRepetition::FORWARD);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 5);
+
+    a1.gm_change_repetition(GMRepetition::BACKWARD);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 8);
+
+    a1.gm_change_repetition(GMRepetition::FORWARD_LOOP);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 5);
+
+    a1.gm_change_repetition(GMRepetition::BACKWARD_LOOP);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 8);
+
+    a1.gm_change_repetition(GMRepetition::PINGPONG_F);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 5);
+
+    a1.gm_change_repetition(GMRepetition::PINGPONG_B);
+    REQUIRE(!a1.gm_finished());
+    REQUIRE(a1.gm_get_frame_index() == 8);
+}
