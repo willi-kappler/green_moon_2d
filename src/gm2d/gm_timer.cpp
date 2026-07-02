63ebe3f61a31e881a04a40fceb8a2cb6574fc14f
Willi Kappler <willi.kappler@uni-tuebingen.de>
2026-07-02 10:50:25 +0200

    Add gm_timer.

---
+++ src/gm2d/gm_timer.cpp
@@ -0,0 +1,51 @@
+/*
+    GreenMoon2D
+    SPDX-License-Identifier: MIT
+    Written by Willi Kappler, MIT License
+    https://github.com/willi-kappler/green_moon_2d
+
+    This file defines the timer class
+*/
+
+
+// Local includes:
+#include "gm_timer.hpp"
+
+namespace gm2d {
+GMTimer::GMTimer():
+    duration(0),
+    start_time(std::chrono::steady_clock::now()),
+    active(false)
+{}
+
+GMTimer::GMTimer(uint32_t d):
+    duration(d),
+    start_time(std::chrono::steady_clock::now()),
+    active(true)
+{}
+
+bool GMTimer::gm_finished() {
+    if (active) {
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time);
+        return elapsed.count() > duration;
+    } else {
+        return false;
+    }
+}
+
+void GMTimer::gm_set_duration(uint32_t d) {
+    duration = d;
+}
+
+void GMTimer::gm_set_duration_restart(uint32_t d) {
+    duration = d;
+    gm_restart();
+}
+
+void GMTimer::gm_restart() {
+    start_time = std::chrono::steady_clock::now();
+    active = true;
+}
+
+}
---
+++ src/gm2d/gm_timer.hpp
@@ -0,0 +1,35 @@
+/*
+    GreenMoon2D
+    SPDX-License-Identifier: MIT
+    Written by Willi Kappler, MIT License
+    https://github.com/willi-kappler/green_moon_2d
+
+    This file defines the timer class
+*/
+
+#ifndef FILE_GM_TIMER_HPP_INCLUDED
+#define FILE_GM_TIMER_HPP_INCLUDED
+
+// STD includes:
+#include <cstdint>
+#include <chrono>
+
+namespace gm2d {
+class GMTimer {
+    // Constructor:
+    GMTimer();
+    GMTimer(uint32_t);
+
+    uint32_t duration;
+    std::chrono::time_point<std::chrono::steady_clock> start_time;
+    bool active;
+
+    // Methods:
+    bool gm_finished();
+    void gm_set_duration(uint32_t);
+    void gm_set_duration_restart(uint32_t);
+    void gm_restart();
+};
+}
+
+#endif // FILE_GM_TIMER_HPP_INCLUDED
