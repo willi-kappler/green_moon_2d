1263519a02db9b0a39156dd6c0d815516213af89
Willi Kappler <willi.kappler.gm@gmail.com>
2026-07-19 16:11:52 +0200

    Add code for bitmap font.

--- src/gm2d/gm_font.cpp
+++ src/gm2d/gm_font.cpp
@@ -11,5 +11,13 @@
 #include "gm_font.hpp"
 
 namespace gm2d {
+GMFont::GMFont(std::string font_name, GMTexture font_texture, std::unordered_map<char, uint16_t> font_mapping):
+    name(font_name),
+    texture(font_texture),
+    mapping(font_mapping)
+{}
 
+void GMFont::gm_draw(std::float32_t x, std::float32_t y, char c) {
+    texture.gm_draw(x, y, mapping[c]);
+}
 }
--- src/gm2d/gm_font.hpp
+++ src/gm2d/gm_font.hpp
@@ -10,11 +10,27 @@
 #ifndef FILE_GM_FONT_HPP_INCLUDED
 #define FILE_GM_FONT_HPP_INCLUDED
 
+// STD includes:
+#include <string>
+#include <stdfloat>
+#include <cstdint>
+#include <unordered_map>
+
 // Local includes:
 #include "gm_texture.hpp"
 
 namespace gm2d {
+class GMFont {
+    public:
+        GMFont(std::string, GMTexture, std::unordered_map<char, uint16_t>);
 
+        void gm_draw(std::float32_t, std::float32_t, char);
+
+    private:
+        std::string name;
+        GMTexture texture;
+        std::unordered_map<char, uint16_t> mapping;
+};
 }
 
 #endif // FILE_GM_FONT_HPP_INCLUDED
