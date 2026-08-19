c50da5539ec20121407902eeea59bd000d0f381d
Willi Kappler <willi.kappler.gm@gmail.com>
2026-08-19 02:17:11 +0200

    Add first code for resources.

--- src/gm2d/gm_resources.hpp
+++ src/gm2d/gm_resources.hpp
@@ -10,12 +10,27 @@
 #ifndef FILE_GM_RESOURCES_HPP_INCLUDED
 #define FILE_GM_RESOURCES_HPP_INCLUDED
 
+// STD includes:
+//#include <cstdint>
+//#include <string_view>
+#include <vector>
+#include <memory>
+#include <filesystem>
+//#include <utility>
+
 // Local includes:
-#include "gm_bitmapfont.hpp"
 #include "gm_texture.hpp"
+#include "gm_bitmapfont.hpp"
 
 namespace gm2d {
+class GMResourceManager {
+    public:
+        GMResourceManager();
 
+        void gm_load_resources(std::filesystem::path);
+
+    private:
+};
 }
 
 #endif // FILE_GM_RESOURCES_HPP_INCLUDED
