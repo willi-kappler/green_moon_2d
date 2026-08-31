b9efc009fc815f6987f6daea6ec45a3a067845ea
Willi Kappler <willi.kappler@uni-tuebingen.de>
2026-08-31 19:25:29 +0200

    Force GCC flags for now.

--- xmake.lua
+++ xmake.lua
@@ -28,42 +28,30 @@ add_rules("mode.debug", "mode.release")
 -- xrepo clean
 -- xrepo update-repo
 
+before_build(function (target)
+    local compiler = target:tool("cxx")
+    if compiler then
+        if compiler:find("gcc") or compiler:find("g%+%+") then
+            cprint("GCC detected: %s", compiler)
+        elseif compiler:find("clang") then
+            cprint("Clang detected: %s", compiler)
+        elseif compiler:find("cl") then
+            cprint("MSVC detected: %s", compiler)
+        end
+    end
+end)
+
 -- For all compilers:
 set_warnings("all", "extra", "pedantic", "error")
+add_cxxflags("-Ofast")
+-- add_cxxflags("-Ofast", "-march=native")
+add_cxxflags("-Walloca", "-Wcast-align=strict", "-Wimplicit-fallthrough=5")
+add_cxxflags("-Wconversion", "-Wshadow", "-Wsign-conversion", "-Wdouble-promotion", "-Wformat=2")
+add_cxxflags("-Wundef", "-Wcast-qual", "-Wnon-virtual-dtor", "-Wold-style-cast")
+add_cxxflags("-Woverloaded-virtual", "-Wunused", "-Wuninitialized", "-Winit-self")
+add_cxxflags("-Wredundant-decls", "-Wsuggest-override")
 
--- For GCC and Clang only:
-if is_kind("gcc", "clang") then
-    -- For GCC only:
-    if is_kind("gcc") then
-        add_cxxflags("-Walloca", "-Wcast-align=strict", "-Wimplicit-fallthrough=5")
-    elseif is_kind("clang") then
-        -- Clang prefers the boolean flag without the numeric level
-        add_cxxflags("-Wimplicit-fallthrough")
-    end
-    -- Optimization:
-    add_cxflags("-march=native", "-Ofast")
 
-    -- Warnings:
---    add_cxxflags("-Wnull-dereference", "-Wswitch-enum")
-    add_cxxflags("-Wconversion", "-Wshadow", "-Wsign-conversion", "-Wdouble-promotion", "-Wformat=2")
-    add_cxxflags("-Wundef", "-Wcast-qual", "-Wnon-virtual-dtor", "-Wold-style-cast")
-    add_cxxflags("-Woverloaded-virtual", "-Wunused", "-Wuninitialized", "-Winit-self")
-    add_cxxflags("-Wredundant-decls", "-Wsuggest-override")
-end
-
--- For MSVC only:
-if is_kind("cl") then
-    -- add_cxxflags("/Wall")
-    add_cxxflags("/W4") -- Level 4 is the standard "Strict" for MSVC
-    add_cxxflags("/w14242") -- 'identifier': conversion from 'type1' to 'type1', possible loss of data
-    add_cxxflags("/w14265") -- 'class': class has virtual functions, but destructor is not virtual
-    add_cxxflags("/w14287") -- 'operator': unsigned/negative constant mismatch
-    add_cxxflags("/we4289") -- nonstandard extension used: 'variable': control variable declared in the for-loop is used outside the for-loop scope
-    add_cxxflags("/w14296") -- 'operator': expression is always false
-    add_cxxflags("/w14311") -- 'variable' : pointer truncation from 'type' to 'type'
-    add_cxxflags("/wd4068") -- disable "unknown pragma" (useful if you use GCC pragmas)
-    add_cxxflags("/utf-8")
-end
 
 add_requires("taocpp-json 2025.03.11")
 add_requires("snitch")
@@ -74,6 +62,7 @@ add_requires("libsdl3_image")
 
 target("green_moon_2d")
     set_kind("shared")
+
     if is_plat("windows") then
         add_rules("utils.symbols.export_all", {export_classes = true})
     end
